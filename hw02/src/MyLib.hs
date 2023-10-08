511634cd99f80b6e229a0be55493f10cb2f85133
Artem Levonovich <113240536+Artem9999999999999999999999@users.noreply.github.com>
2023-10-08 16:11:10 +0300

    Add files via upload

---
+++ hw02/src/MyLib.hs
@@ -0,0 +1,189 @@
+module MyLib where
+
+
+-- импортируем foldl', чтобы можно было вызывать как L.foldl'
+import qualified Data.List as L (foldl', foldr)
+
+------------------------------------------------------------------------------------------------
+
+-- 1. traceFoldl (0,25 балла)
+
+-- Клёвый пакет для дебага хаскельных программ.
+-- Например, функция traceShow имеет тип Show a => a -> b -> b.
+-- Первым аргументом в неё нужно передать то, что хочется вывести на экран.
+-- В остальном она работет так же, как и id. 
+-- Поэтому её можно воткнуть в любое место в программе.
+import Debug.Trace (traceShow)
+
+-- Пример
+traceFoldr :: Show b => (a -> b -> b) -> b -> [a] -> b
+traceFoldr _ acc []       = acc
+traceFoldr f acc (x : xs) =
+  let res = traceFoldr f acc xs
+   in f x (traceShow res res)
+
+-- | Напишите traceFoldl, аналогичную traceFoldr,
+--   которая с помощью traceShow печатает значение аккумулятора на каждом шаге
+--
+
+traceFoldl :: Show b => (b -> a -> b) -> b -> [a] -> b
+traceFoldl _ acc []       = acc
+traceFoldl f acc (x : xs) = 
+    let newAcc = f acc x
+     in traceShow newAcc (traceFoldl f newAcc xs)
+
+
+------------------------------------------------------------------------------------------------
+
+-- 2. Реализуйте следующие функции с ипользованием свертки (2,5 балла = 0,25 * 10)
+-- Подумайте, какую функцию сверки когда лучше использовать
+
+or' :: [Bool] -> Bool 
+or' = L.foldr (\x acc -> x || acc) False
+
+length' :: [a] -> Int
+length' x = L.foldl' (\count _ -> count + 1) 0 x
+
+maximum' :: [Int] -> Maybe Int
+maximum'[]        = Nothing
+maximum' (x : xs) = Just 
+                        (L.foldl'
+                            (\maxVal elem -> if elem > maxVal
+                                then elem
+                                    else maxVal) x xs)
+
+reverse' :: [a] -> [a]
+reverse' = L.foldl' (\x acc -> acc : x) []
+
+-- Если порядок на выходе не важен
+filterl' :: (a -> Bool) -> [a] -> [a]
+filterl' predicate = L.foldl' (\acc x -> if predicate x then x : acc else acc) []
+
+-- Если порядок на выходе важен
+filterr' :: (a -> Bool) -> [a] -> [a]
+filterr' predicate = L.foldr (\x acc -> if predicate x then x : acc else acc) []
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = L.foldr (\x acc -> f x : acc) []
+
+head' :: [a] -> Maybe a
+head' = L.foldr (\x _ -> Just x) Nothing
+
+last' :: [a] -> Maybe a
+last' [] = Nothing
+last' (x : xs) = Just(L.foldl' (\_ elem -> elem) x xs)
+
+-- используйте L.foldl'
+take' :: Int -> [a] -> [a]
+take' n = L.foldl'
+            (\acc x -> if length acc < n
+                then acc ++ [x] 
+                    else acc) []
+
+-- используйте foldr
+take'' :: Int -> [a] -> [a]
+take'' n = L.foldr (\x acc -> if length acc < n then x : acc else acc) []
+
+------------------------------------------------------------------------------------------------
+
+-- 3. Сортировки (2,5 балла = 1 + 1 + 0,5)
+
+-- | Быстрая сортировка (1 балл)
+--
+quicksort :: [Int] -> [Int]
+quicksort [] = []
+quicksort (x : xs) = quicksort [y | y <- xs, y <= x] 
+    ++ [x]
+    ++ quicksort [y | y <- xs, y > x] 
+
+-- | Функция, которая вставляет в уже отсортированный список элементов
+--   новый элемент на такую позицию, что все элементы левее будут меньше или
+--   равны нового элемента, а все элементы справа будут строго больше
+--   (1 балл)
+-- Изменил тип функции, потому что иначе она была несовместима с insertionSort
+--
+insert :: Ord a => [a] -> a -> [a]
+insert [] y = [y]
+insert (x : xs) y
+    | y < x     = y : x : xs
+    | otherwise = x: insert xs y
+
+-- | Сортировка вставками (0,5 балла)
+--   В реализации можно использовать функцию insert, если вы её реализовали
+--
+insertionSort :: Ord a => [a] -> [a]
+insertionSort [] = []
+insertionSort (x:xs) = insert (insertionSort xs) x
+
+
+
+
+------------------------------------------------------------------------------------------------
+
+-- 4. Числа Фибоначчи (2,5 баллa = 0,5 + 1 + 1)
+
+-- | аналогична zip, но применяет к элементам пары функцию (0,5 балла)
+--
+myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith _ [] _ = []
+myZipWith _ _ [] = []
+myZipWith f (x : xs) (y : ys) = f x y : myZipWith f xs ys
+
+-- | В Haskell можно создавать бесконечные списки.
+--
+infList :: [Int]
+infList = helper 0
+  where
+    helper :: Int -> [Int]
+    helper x = x : helper (x + 1)
+
+-- | Из-за ленивости они будут работать.
+--   В том плане, что они будут вычисляться ровно до того момента, до которого нам надо.
+-- 
+vals0123 :: [Int]
+vals0123 = take 4 infList -- вернет [0..3]
+
+-- | Задайте бесконечный список всех чисел Фибоначчи (1 балл)
+--   Элементы списка должны вчисляться за O(n), где n — номер числа Фибоначчи
+--   Подсказка: используйте myZipWith
+--
+fibs :: [Integer]
+fibs = 0 : 1 : myZipWith (+) fibs (tail fibs)
+
+-- | Считает сумму всех элементов списка, которые находятся на позиции, 
+--     __индекс__ которой является числом Фибоначчи. (1 балл)
+--
+fibSum :: [Int] -> Int
+fibSum xs = sum [x | (i, x) <- zip [0..] xs, isFibonacciIndex i]
+  where
+    isFibonacciIndex i = isPerfectSquare (5 * i * i + 4) || isPerfectSquare (5 * i * i - 4)
+    isPerfectSquare n = let root = round (sqrt (fromIntegral n))
+                       in root * root == n
+------------------------------------------------------------------------------------------------
+
+-- 6. Деревья (2,25 балла = 0,25 + 1 + 1)
+
+-- | Реализуйте тип данных 'Tree' — дерева, у узлов которого может быть произвольное
+--   число потомков. (0,25 балла)
+--
+data Tree a = Node a [Tree a] -- 'YourTree' — просто заглушка. Вместо неё вам надо будет написать своё описание типа данных 'Tree'
+
+-- | Принимает дерево, а возвращает список значений в его нодах в порядке обхода в ширину (1 балл)
+--
+bfs :: Tree a -> [a]
+bfs tree = bfsHelper [tree] []
+  where
+    bfsHelper [] acc = reverse acc
+    bfsHelper (Node x children : rest) acc =
+      bfsHelper (rest ++ children) (x : acc)
+
+-- | Принимает дерево, а возвращает список значений в его нодах в порядке обхода в глубину (1 балл)
+--
+dfs :: Tree a -> [a]
+dfs tree = dfsHelper [tree] []
+  where
+    dfsHelper [] acc = reverse acc
+    dfsHelper (Node x children : rest) acc =
+      dfsHelper (children ++ rest) (x : acc)
+
+------------------------------------------------------------------------------------------------
---
+++ hw02/test/TestSpec.hs
@@ -0,0 +1,215 @@
+module TestSpec (spec) where
+
+import MyLib
+
+import Test.Hspec
+  (
+    Spec
+  , it
+  , shouldBe
+  , describe
+  , shouldMatchList
+  )
+
+spec :: Spec
+spec = do
+  describe "test `or'" $ do
+    it "empty list" $ do
+      or' [] `shouldBe` (False :: Bool)
+  
+    it "True item" $ do
+      or [True, False] `shouldBe` (True :: Bool)
+
+    it "False items" $ do
+      or' [False, False] `shouldBe` (False :: Bool)
+
+
+  describe "test `lenght'" $ do
+    it "empty list" $ do
+      length' [] `shouldBe` (0 :: Int)
+  
+    it "1 item" $ do
+      length' [1] `shouldBe` (1 :: Int)
+
+    it "Many items" $ do
+      length' [1..100] `shouldBe` (100 :: Int)
+
+
+  describe "test `maximum'" $ do
+    it "empty list" $ do
+      maximum' [] `shouldBe` (Nothing :: Maybe Int)
+  
+    it "1 item" $ do
+      maximum' [1213] `shouldBe` (Just 1213 :: Maybe Int)
+
+    it "Many items" $ do
+      maximum' [1109, 1200, 23423, 354, 39, 2, 1, 459] `shouldBe` (Just 23423 :: Maybe Int)
+
+
+  describe "test `reverse'" $ do
+    it "empty list" $ do
+      reverse' [] `shouldBe` ([] :: [Int])
+  
+    it "1 item" $ do
+      reverse' [1213, 943..200] `shouldBe` ([403,673,943,1213] :: [Int])
+
+    it "Many items" $ do
+      reverse' [200, 1109..4591] `shouldBe` ([3836,2927,2018,1109,200] :: [Int])
+
+
+  describe "test `filterl'" $ do
+    it "case 2" $ do
+      filterl' (\x -> x > 0) [-2, 0, 5, -10, 7] `shouldMatchList` ([5, 7] :: [Int])
+
+    it "case 3" $ do
+      filterl' null [[1, 2], [], [3, 4], []] `shouldMatchList` ([[], []] :: [[Int]])
+
+    it "case 4" $ do
+      filterl' (\x -> x == 3) [3, 2, 3, 1, 3] `shouldMatchList` ([3, 3, 3] :: [Int])
+
+  describe "test `filterr'" $ do
+
+    it "case 2" $ do
+      filterr' (\x -> x > 0) [-2, 0, 5, -10, 7] `shouldMatchList` ([5, 7] :: [Int])
+
+    it "case 3" $ do
+      filterr' null [[1, 2], [], [3, 4], []] `shouldMatchList` ([[], []] :: [[Int]])
+
+    it "case 4" $ do
+      filterr' (\x -> x == 3) [3, 2, 3, 1, 3] `shouldMatchList` ([3, 3, 3] :: [Int])
+
+  describe "test `map'" $ do
+    it "case 1" $ do
+      map' (\x -> x * 2) [1, 2, 3, 4, 5] `shouldBe` ([2, 4, 6, 8, 10] :: [Int])
+  
+    it "case 2" $ do
+      map' show [1, 2, 3, 4, 5] `shouldBe` (["1", "2", "3", "4", "5"] :: [String])
+
+
+  describe "test head'" $ do
+    it "empty list" $ do
+     head' [] `shouldBe` (Nothing :: Maybe Int)
+  
+    it "1 item" $ do
+      head' [1213] `shouldBe` (Just 1213 :: Maybe Int)
+
+    it "many items" $ do
+      head' [1200, 1109..459] `shouldBe` (Just 1200 :: Maybe Int)
+
+
+  describe "test last'" $ do
+    it "empty list" $ do
+     last' [] `shouldBe` (Nothing :: Maybe Int)
+  
+    it "1 item" $ do
+      last' [1213] `shouldBe` (Just 1213 :: Maybe Int)
+
+    it "many items" $ do
+      last' [1200, 1109..459] `shouldBe` (Just 472 :: Maybe Int)
+
+
+  describe "test take'" $ do
+    it "1 item" $ do
+      take' 1 [1213] `shouldBe` ([1213] :: [Int])
+
+    it "many items" $ do
+      take' 2 [1200, 1109..459] `shouldBe` ([1200, 1109] :: [Int])
+
+
+  describe "test take''" $ do
+    it "1 item" $ do
+      take'' 1 [1213] `shouldBe` ([1213] :: [Int])
+
+    it "many items" $ do
+      take'' 2 [1200, 1109..459] `shouldBe` ([563,472] :: [Int])
+
+
+  describe "test quicksort" $ do
+    it "case 1" $ do
+      quicksort [3, 4, 1, 9, 2, 6, 3, 5] `shouldBe` ([1,2,3,3,4,5,6,9] :: [Int])
+
+    it "case 2" $ do
+      quicksort [42] `shouldBe` ([42] :: [Int])
+
+    it "case 3" $ do
+      quicksort [] `shouldBe` ([] :: [Int])
+
+    it "case 4" $ do
+      quicksort [5, 5, 5] `shouldBe` ([5, 5, 5] :: [Int])
+
+  describe "test insert" $ do
+    it "case 1" $ do
+      insert [1, 2, 3, 5, 6] 4 `shouldBe` ([1, 2, 3, 4, 5, 6] :: [Int])
+
+    it "case 2" $ do
+      insert [] 4 `shouldBe` ([4] :: [Int])
+
+    it "case 3" $ do
+      insert [4] 4 `shouldBe` ([4, 4] :: [Int])
+
+
+  describe "test insertionSort" $ do
+    it "case 1" $ do
+      insertionSort [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5] `shouldBe` ([1,1,2,3,3,4,5,5,5,6,9] :: [Int])
+
+    it "case 2" $ do
+      insertionSort [42] `shouldBe` ([42] :: [Int])
+
+    it "case 3" $ do
+      insertionSort [] `shouldBe` ([] :: [Int])
+
+    it "case 4" $ do
+      insertionSort [5, 5, 5, 5, 5] `shouldBe` ([5, 5, 5, 5, 5] :: [Int])
+
+
+  describe "test myZipWith" $ do
+    it "case 1" $ do
+      myZipWith (+) [1, 2, 3] [4, 5, 6] `shouldBe` ([5, 7, 9] :: [Int])
+
+    it "case 2" $ do
+      myZipWith (*) [1, 2, 3] [4, 5, 6] `shouldBe` ([4, 10, 18] :: [Int])
+
+    it "case 3" $ do
+      myZipWith (\x y -> x * y + 1) [1, 2, 3] [4, 5, 6] `shouldBe` ([5, 11, 19] :: [Int])
+
+    it "case 4" $ do
+      myZipWith (\x y -> x + y) [1, 2] [4, 5, 6] `shouldBe` ([5, 7] :: [Int])
+
+    it "case 5" $ do
+      myZipWith (\x y -> x * y) [1, 2, 3] [] `shouldBe` ([] :: [Int])
+
+
+  describe "test fibs" $ do
+    it "case 1" $ do
+      take 10 fibs `shouldBe` ([0, 1, 1, 2, 3, 5, 8, 13, 21, 34] :: [Integer])
+
+    it "case 2" $ do
+      take 5 (drop 5 fibs) `shouldBe` ([5, 8, 13, 21, 34] :: [Integer])
+
+
+  describe "test fibSum" $ do
+    it "case 1" $ do
+      fibSum [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] `shouldBe` (25 :: Int)
+
+    it "case 2" $ do
+      fibSum [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55] `shouldBe` (30 :: Int)
+
+
+  describe "test bfs" $ do
+    it "case 1" $ do
+      let tree = Node 1 [Node 2 [Node 3 [], Node 4 []], Node 5 [Node 6 [], Node 7 []]]
+      bfs tree `shouldBe` ([1, 2, 5, 3, 4, 6, 7] :: [Int])
+
+    it "case 2" $ do
+      let tree = Node 'A' [Node 'B' [Node 'C' [], Node 'D' []], Node 'E' [Node 'F' []]]
+      bfs tree `shouldBe` ("ABECDF" :: [Char])
+
+
+  describe "test dfs" $ do
+    it "case 1" $ do
+      let tree = Node 1 [Node 2 [Node 3 [], Node 4 []], Node 5 [Node 6 [], Node 7 []]]
+      dfs tree `shouldBe` ([1, 2, 3, 4, 5, 6, 7] :: [Int])
+
+    it "case 2" $ do
+      let tree = Node 'A' [Node 'B' [Node 'C' [], Node 'D' []], Node 'E' [Node 'F' []]]
+      dfs tree `shouldBe` ("ABCDEF" :: [Char])
