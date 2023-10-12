f0926795467f51e0dd37e728837182479d827440
Ivan <113239177+paiksonn@users.noreply.github.com>
2023-10-12 21:24:34 +0300

    Update TestSpec.hs

--- test/TestSpec.hs
+++ test/TestSpec.hs
@@ -1,6 +1,24 @@
 module TestSpec (spec) where
 
 import MyLib
+    ( dfs,
+      bfs,
+      fibs,
+      myZipWith,
+      insertionSort,
+      insert,
+      quicksort,
+      take'',
+      take',
+      last',
+      head',
+      map',
+      filter',
+      reverse',
+      maximum',
+      length',
+      or',
+      Tree(Node) )
 
 import Test.Hspec
   (
@@ -18,10 +36,10 @@ spec = do
       or' [] `shouldBe` (False :: Bool)
 
     it "True item1" $ do
-      or [True, False] `shouldBe` (True :: Bool)
+      or' [True, False] `shouldBe` (True :: Bool)
 
     it "True item2" $ do
-      or [True, True] `shouldBe` (True :: Bool)
+      or' [True, True] `shouldBe` (True :: Bool)
 
     it "False items" $ do
       or' [False, False] `shouldBe` (False :: Bool)
@@ -60,20 +78,22 @@ spec = do
 
 
   describe "test `filter'" $ do
-    it "Case 1" $ do
-      filter' (\x -> x > 0) [10, -3, 33, -1, -22] `shouldMatchList` ([10, 33] :: [Int])
+    it ">0" $ do
+      filter' (> 0) [10, -3, 33, -1, -22] `shouldBe` ([10, 33] :: [Int])
       -- а можно просто > 0
 
-    it "Case 2" $ do
-      filter' null [[1, 1, 1, 1], [], [2, 3], []] `shouldMatchList` ([[], []] :: [[Int]])
+    it "null at the end" $ do
+      filter' null [[1, 1, 1, 1], [], [2, 3], []] `shouldBe` ([[], []] :: [[Int]])
+
+    it "equality to 8" $ do
+      filter' (==8) [8, 9, 2, 2, 3, 3, 8, 8, 8, 8] `shouldBe` ([8, 8, 8, 8, 8] :: [[Int]])
 
 
   describe "test `map'" $ do
-    it "Case 1" $ do
-      map' (\x -> x * x) [1, 2, 3, 4] `shouldBe` ([1, 4, 9, 16] :: [Int])
-      -- а можно просто * x
+    it "*x" $ do
+      map' (*x) [1, 2, 3, 4] `shouldBe` ([1, 4, 9, 16] :: [Int])
 
-    it "Case 2" $ do
+    it "Let it SHOW" $ do
       map' show [0, 1, 0, 1] `shouldBe` (["0", "1", "0", "1"] :: [String])
 
 
@@ -116,47 +136,53 @@ spec = do
 
 
   describe "test quicksort" $ do
-    it "Case 1" $ do
+    it "Empty" $ do
         quicksort [] `shouldBe` ([] :: [Int])
 
-    it "Case 2" $ do
+    it "1 element sort" $ do
         quicksort [42] `shouldBe` ([42] :: [Int])
 
-    it "Case 3" $ do
+    it "Bag of elements" $ do
         quicksort [3, 2, 2, 2, 1] `shouldBe` ([1, 2, 2, 2, 3] :: [Int])
+
+    it "Same elements" $ do
+        quicksort [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] `shouldBe` ([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] :: [Int])
 
 
   describe "test insert" $ do
-    it "Case 1" $ do
+    it "Empty" $ do
         insert [] 1 `shouldBe` ([1] :: [Int])
 
-    it "Case 2" $ do
+    it "Usual numbers" $ do
         insert [1, 2, 3, 4, 6] 5 `shouldBe` ([1, 2, 3, 4, 5, 6] :: [Int])
 
 
   describe "test insertionSort" $ do
-    it "Case 1" $ do
+    it "Empty" $ do
         insertionSort [] `shouldBe` ([] :: [Int])
 
-    it "Case 2" $ do
+    it "1 element" $ do
         insertionSort [1] `shouldBe` ([1] :: [Int])
 
-    it "Case 3" $ do
+    it "Normal numbers" $ do
         insertionSort [3, 2, 2, 2, 1] `shouldBe` ([1, 2, 2, 2, 3] :: [Int])
+
+    it "Same elements" $ do
+        insertionSort [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] `shouldBe` ([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] :: [Int])
 
 
   describe "test myZipWith" $ do
-    it "Case 1" $ do
+    it "normal +" $ do
         myZipWith (+) [1, 2, 3] [4, 5, 6] `shouldBe` ([5, 7, 9] :: [Int])
 
-    it "Case 2" $ do
+    it "normal *" $ do
         myZipWith (*) [1, 2, 3] [4, 5, 6] `shouldBe` ([4, 10, 18] :: [Int])
 
-    it "Case 3" $ do
-        myZipWith (\x y -> x + y) [1, 2] [3, 4, 5, 6, 7] `shouldBe` ([4, 6] :: [Int])
+    it "Defferent +" $ do
+        myZipWith (+) [1, 2] [3, 4, 5, 6, 7] `shouldBe` ([4, 6] :: [Int])
 
-    it "Case 4" $ do
-        myZipWith (\x y -> x * y) [1, 2, 3] [] `shouldBe` ([] :: [Int])
+    it "Zero *" $ do
+        myZipWith (*) [1, 2, 3] [] `shouldBe` ([] :: [Int])
 
 
   describe "test fibs" $ do
@@ -169,8 +195,40 @@ spec = do
       let tree = Node 1 [Node 2 [Node 3 []], Node 4 [Node 5 [], Node 6 []]]
       bfs tree `shouldBe` ([1, 2, 4, 3, 5, 6] :: [Int])
 
+    it "AAAAAAAAAAAAAAAAAAA" $ do
+      let tree = Node 'A' [Node 'B' [Node 'C' [], Node 'D' []], Node 'E' [Node 'F' []]]
+      bfs tree `shouldBe` ("ABECDF" :: [Char])
+
+    it "OP OP OP OP OP OP OP OP OP OP OP OP OP" $ do
+      let tree = Node 0 [Node 1 [Node 2 [Node 3 []]], Node 4 [Node 5 []]]
+      bfs tree `shouldBe` ([0, 1, 4, 2, 5, 3] :: [Int])
+
+    it "XYZ" $ do
+      let tree = Node 'X' [Node 'Y' [], Node 'Z' [Node '!' []]]
+      bfs tree `shouldBe` ("XYZ!" :: [Char])
+
+    it "11111111111111111111111" $ do
+      let tree = Node 696969696996969696969969696969699696 []
+      bfs tree `shouldBe` ([696969696996969696969969696969699696] :: [Integer])
+
 
   describe "test dfs" $ do
     it "THE END" $ do
       let tree = Node 1 [Node 2 [Node 3 [], Node 4 []], Node 5 [Node 6 [], Node 7 []]]
       dfs tree `shouldBe` ([1, 2, 3, 4, 5, 6, 7] :: [Int])
+
+    it "AAAAAAAAAAAAAAAAAAA" $ do
+      let tree = Node 'F' [Node 'U' [Node 'N' [], Node 'N' []], Node 'Y' [Node '!' []]]
+      bfs tree `shouldBe` ("FUNNY!" :: [Char])
+
+    it "OP OP OP OP OP OP OP OP OP OP OP OP OP" $ do
+      let tree = Node 0 [Node 1 [Node 2 [Node 3 []]], Node 4 [Node 5 []]]
+      bfs tree `shouldBe` ([0, 1, 2, 3, 4, 5] :: [Int])
+
+    it "XYZ" $ do
+      let tree = Node 'X' [Node 'Y' [Node 'W' []], Node 'Z' []]
+      bfs tree `shouldBe` ("XYWZ" :: [Char])
+
+    it "11111111111111111111111" $ do
+      let tree = Node 696969696996969696969969696969699696 []
+      bfs tree `shouldBe` ([696969696996969696969969696969699696] :: [Integer])
