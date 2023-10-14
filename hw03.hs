651cd9b2e108baa81eb46834c35a98e981879464
pluralia <irina.artemeva@typefox.io>
2023-10-14 17:34:21 +0200

    Add hw03

---
+++ hw03.hs
@@ -0,0 +1,198 @@
+import Data.Ix (inRange)
+
+------------------------------------------------------------------------------------------------
+
+-- 1. Числа Черча и `Ix` (1 балл)
+--    Напишите инстансы `Eq`, `Ord`, `Num` и `Ix` для чисел Черча
+data ChurchNumber = Zero | Succ ChurchNumber
+  deriving (Show, Eq)
+
+-- Вы можете найти класс `Ix` по ссылке:
+-- https://hackage.haskell.org/package/base-4.19.0.0/docs/Data-Ix.html
+-- Обратите внимание на необходимость импорта: `import Data.Ix`
+
+------------------------------------------------------------------------------------------------
+
+-- 2. Дерево (2 балла)
+
+data Tree a = Node
+  { value :: a
+  , children :: [Tree a]
+  }
+
+---------------------------------------
+
+-- 2.a Напишите 3 инстанса `Show` для дерева.
+--     Истансы должны реализовывать in-, pre-, post-order обход дерева (1,5 балла)
+
+-- Мы не можем определять несколько инстансов одного типа для одного класса,
+-- поэтому обернем наш тип `Tree` в newtype, чтобы у нас было 3 разных типа,
+-- и уже для них реализуем `Show`
+
+-- | in-order обход дерева
+--
+newtype InOrder a = In (Tree a)
+
+instance Show a => Show (InOrder a) where
+    show :: Show a => InOrder a -> String
+    show = undefined
+
+-- | pre-order обход дерева
+--
+newtype PreOrder a = Pre (Tree a)
+
+instance Show a => Show (PreOrder a) where
+    show :: Show a => PreOrder a -> String
+    show = undefined
+
+-- | post-order обход дерева
+--
+newtype PostOrder a = Post (Tree a)
+
+instance Show a => Show (PostOrder a) where
+    show :: Show a => PostOrder a -> String
+    show = undefined
+
+---------------------------------------
+
+-- 2.b Напишите инстанс `Eq` для дерева (0,5 балла)
+
+------------------------------------------------------------------------------------------------
+
+-- 3. Цвета (2 балла)
+
+-- | Зададим тип данных RGB, но так как его поля должны быть от 0 до 255, назовем конструктор Unsafe...
+--
+data RGB = UnsafeMkRGB
+  { red   :: Int
+  , green :: Int
+  , blue  :: Int
+  } deriving (Show)
+
+-- | ...и зададим новый конструктор, который будет проверять значения полей при инициализации
+--
+mkRGB :: Int -> Int -> Int -> Maybe RGB
+mkRGB red green blue
+  | inRange (0, 255) `all` [red, green, blue] = Just $ UnsafeMkRGB red green blue
+  | otherwise                                 = Nothing 
+
+-- | Аналогично поступим, задавая тип данных CMYK
+--
+data CMYK = UnsafeMkCMYK
+  { cyan    :: Int
+  , magenta :: Int
+  , yellow  :: Int
+  , black   :: Int
+  } deriving (Show)
+
+mkCMYK :: Int -> Int -> Int -> Int -> Maybe CMYK
+mkCMYK cyan magenta yellow black
+  | inRange (0, 100) `all` [cyan, magenta, yellow, black] = Just $ UnsafeMkCMYK cyan magenta yellow black
+  | otherwise                                             = Nothing 
+
+---------------------------------------
+
+-- 3.a Напишите инстансы класса ToCMYK для [Int] и для RGB (0,75 балла)
+
+class ToCMYK a where
+    toCMYK :: a -> Maybe CMYK
+
+---------------------------------------
+
+-- 3.b Классы типов -- это функции (1 балл)
+
+-- | задайте класс типов ToCMYK используя data (аналогично Eq' из практики)
+--
+data DToCMYK = Placeholder
+
+-- | реализуйте класс типов DToCMYK для [Int] и для RGB в виде функций (аналогично dEqBool из практики)
+
+---------------------------------------
+
+-- 3.c Используйте инстансы (0,25 балла)
+--     Приведите пример использования инстансов [Int] и RGB реализованных в 3a и 3b (должно получится 4 примера)
+
+------------------------------------------------------------------------------------------------
+
+-- 4. Сделайте функцию `pointful` бесточечной, объясняя каждый шаг по примеру из практики
+--    (1,5 балла)
+
+pointful a b c d = a b (c d)
+
+------------------------------------------------------------------------------------------------
+
+-- 5. Дни недели и `Enum` (1 балл)
+
+---------------------------------------
+
+-- 5.a Задайте тип данных "дни недели", в котором перечислите дни недели
+--     и реализуйте для него класс типов `Enum`
+--     https://hackage.haskell.org/package/base-4.19.0.0/docs/Prelude.html#t:Enum
+
+data Day = MyDaysAreHere
+
+---------------------------------------
+
+-- 5.b Реализуйте следующие функции
+
+-- | Возвращает следующий день
+--
+nextDay :: Day -> Day
+nextDay = undefined
+
+-- | Возвращает предыдущий день
+--
+dayBefore :: Day -> Day
+dayBefore = undefined
+
+-- | Возвращает количество от текущего до ближайшей субботы
+--
+daysBeforeWeekend :: Day -> Int
+daysBeforeWeekend = undefined
+
+------------------------------------------------------------------------------------------------
+
+-- 6. Класс типов `Functor` (1,75 балла)
+
+-- Вы уже знакомы с функцией `map` -- мы писали ее для различных типов данных (mapMaybe, mapList)
+-- На самом деле она задана в классе типов `Functor`
+-- https://hackage.haskell.org/package/base-4.19.0.0/docs/Data-Functor.html
+
+---------------------------------------
+
+-- 6.a Реализуйте инстанс Functor для списка (0,25 балла)
+
+data List a = Nil | Cons a (List a)
+  deriving (Show)
+
+instance Functor List where
+  fmap :: (a -> b) -> List a -> List b
+  fmap = undefined
+
+---------------------------------------
+
+-- 6.b Реализуйте инстанс Functor для дерева из задания 2 (0,5 балла)
+
+---------------------------------------
+
+-- 6.c(*) Реализуйте инстанс Functor для пары (1 балл)
+
+data Pair a b = Pair a b
+  deriving (Show)
+
+-- С какими трудностями вы столкнулись?
+
+------------------------------------------------------------------------------------------------
+
+-- 7. Класс типов `Bifunctor` (0,5 балла)
+
+-- Вы реализовывали функцию mapEither -- она аналочна функции bimap из Bifunctor
+-- https://hackage.haskell.org/package/base-4.19.0.0/docs/Data-Bifunctor.html#t:Bifunctor
+
+-- Реализуйте инстанс Functor для Either и пары
+
+------------------------------------------------------------------------------------------------
+
+-- 8. Задайте свой класс типов, опираясь на интересы, хобби и фазу Луны (0,25 балла)
+
+------------------------------------------------------------------------------------------------
