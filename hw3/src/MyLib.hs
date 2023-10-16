df077fc718addf88c976a7db0bde41e7bf8e395c
qilowoq <klyambus@gmail.com>
2023-10-16 20:22:51 +0300

    mark I

--- hw3/src/MyLib.hs
+++ hw3/src/MyLib.hs
@@ -1,17 +1,107 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
+
 module MyLib where
 
-import Data.Ix (inRange)
+import Data.Bifunctor
+import Data.Ix
+import Data.List as L
+import Data.Set as S
+
+default (Int, Double)
 
 ------------------------------------------------------------------------------------------------
 
 -- 1. Числа Черча и `Ix` (1 балл)
 --    Напишите инстансы `Eq`, `Ord`, `Num` и `Ix` для чисел Черча
 data ChurchNumber = Zero | Succ ChurchNumber
-  deriving (Show, Eq)
+  deriving (Show)
 
 -- Вы можете найти класс `Ix` по ссылке:
 -- https://hackage.haskell.org/package/base-4.19.0.0/docs/Data-Ix.html
 -- Обратите внимание на необходимость импорта: `import Data.Ix`
+
+instance Eq ChurchNumber where
+  (==) :: ChurchNumber -> ChurchNumber -> Bool
+  Zero == Zero = True
+  Zero == _ = False
+  _ == Zero = False
+  Succ x == Succ y = x == y
+
+instance Ord ChurchNumber where
+  compare :: ChurchNumber -> ChurchNumber -> Ordering
+  compare Zero Zero = EQ
+  compare Zero (Succ _) = LT
+  compare (Succ _) Zero = GT
+  compare (Succ x) (Succ y) = compare x y
+
+-- Возьмем реализованные функции из hw01
+chAdd :: ChurchNumber -> ChurchNumber -> ChurchNumber
+chAdd x Zero = x
+chAdd x (Succ y) = chAdd (Succ x) y
+
+chMult :: ChurchNumber -> ChurchNumber -> ChurchNumber
+chMult _ Zero = Zero
+chMult x (Succ Zero) = x
+chMult x (Succ y) = chAdd x (chMult x y)
+
+chPrev :: ChurchNumber -> ChurchNumber
+chPrev (Succ x) = x
+chPrev Zero = Zero
+
+instance Num ChurchNumber where
+  (+) :: ChurchNumber -> ChurchNumber -> ChurchNumber
+  x + y = chAdd x y
+
+  (-) :: ChurchNumber -> ChurchNumber -> ChurchNumber
+  x - Zero = x
+  Zero - _ = Zero -- наверное это правильно. у нас ведь нет отрицательных чисел
+  x - y = chPrev x - chPrev y
+
+  (*) :: ChurchNumber -> ChurchNumber -> ChurchNumber
+  x * y = chMult x y
+
+  fromInteger :: Integer -> ChurchNumber
+  fromInteger = helper Zero
+   where
+    helper :: ChurchNumber -> Integer -> ChurchNumber
+    helper acc n
+      | n <= 0 = acc
+      | otherwise = helper (Succ acc) (n - 1)
+
+  abs :: ChurchNumber -> ChurchNumber -- Заглушка для warning
+  abs = id
+
+  signum :: ChurchNumber -> ChurchNumber -- Заглушка для warning
+  signum = id
+
+instance Ix ChurchNumber where
+  range :: (ChurchNumber, ChurchNumber) -> [ChurchNumber]
+  range = helper []
+   where
+    helper :: [ChurchNumber] -> (ChurchNumber, ChurchNumber) -> [ChurchNumber]
+    helper acc (l, r)
+      | l > r = acc
+      | l == r = r : acc
+      | l < r = helper (r : acc) (l, chPrev r)
+
+  inRange :: (ChurchNumber, ChurchNumber) -> ChurchNumber -> Bool
+  inRange (l, r) x
+    | (x >= r) && (x <= l) = True
+    | otherwise = False
+
+  index :: (ChurchNumber, ChurchNumber) -> ChurchNumber -> Int
+  index = helper 0
+   where
+    helper :: Int -> (ChurchNumber, ChurchNumber) -> ChurchNumber -> Int
+    helper ind (l, r) x
+      | x < l = -1
+      | x > r = -1
+      | x == l = ind
+      | otherwise = helper (ind + 1) (l + Succ Zero, r) x
+
+cn :: Integer -> ChurchNumber -- Чтобы было удобно дебажить
+cn = fromInteger -- тк по дефолту fromInteger выдает Int
 
 ------------------------------------------------------------------------------------------------
 
@@ -21,6 +111,7 @@ data Tree a = Node
   { value :: a
   , children :: [Tree a]
   }
+  deriving (Show)
 
 ---------------------------------------
 
@@ -31,95 +122,188 @@ data Tree a = Node
 -- поэтому обернем наш тип `Tree` в newtype, чтобы у нас было 3 разных типа,
 -- и уже для них реализуем `Show`
 
--- | in-order обход дерева
---
-newtype InOrder a = In (Tree a)
-
-instance Show a => Show (InOrder a) where
-    show :: Show a => InOrder a -> String
-    show = undefined
+instance {-# OVERLAPPING #-} Show String where
+  -- Стандартный Show пишет полную чушь:
+  -- ghci> show ["1", "2"]
+  -- "[\"1\", \"2\"]"
+  show :: String -> String
+  show = id
 
 -- | pre-order обход дерева
---
 newtype PreOrder a = Pre (Tree a)
 
-instance Show a => Show (PreOrder a) where
-    show :: Show a => PreOrder a -> String
-    show = undefined
+instance (Show a) => Show (PreOrder a) where
+  show :: (Show a) => PreOrder a -> String
+  show (Pre tree) = show (helper [tree])
+   where
+    helper :: [Tree a] -> [a]
+    helper [] = []
+    helper (node : nodes) = value node : helper (children node) ++ helper nodes
+
+-- | in-order обход дерева
+newtype InOrder a = In (Tree a)
+
+instance (Show a) => Show (InOrder a) where
+  show :: (Show a) => InOrder a -> String
+  show (In tree) = show (helper [tree])
+   where
+    helper :: [Tree a] -> [a]
+    helper [] = []
+    helper (node : nodes)
+      | L.null (children node) = value node : helper nodes
+      | otherwise = (helper . L.singleton . head) (children node) ++ [value node] ++ (helper . tail) (children node) ++ helper nodes
 
 -- | post-order обход дерева
---
 newtype PostOrder a = Post (Tree a)
 
-instance Show a => Show (PostOrder a) where
-    show :: Show a => PostOrder a -> String
-    show = undefined
+instance (Show a) => Show (PostOrder a) where
+  show :: (Show a) => PostOrder a -> String
+  show (Post tree) = show (helper [tree])
+   where
+    helper :: [Tree a] -> [a]
+    helper [] = []
+    helper (node : nodes) = helper (children node) ++ [value node] ++ helper nodes
 
 ---------------------------------------
 
 -- 2.b Напишите инстанс `Eq` для дерева (0,5 балла)
+
+instance (Ord a) => Ord (Tree a) where
+  compare :: Tree a -> Tree a -> Ordering
+  compare x y = compare (value x) (value y)
+
+instance (Ord a) => Eq (Tree a) where
+  (==) :: Tree a -> Tree a -> Bool
+  -- сделал так, что порядок children не важен (изоморфизм или типо того)
+  -- Но S.fromList требует instance Ord (Tree a)
+  -- А есть ли какие-то способы создания сета где не надо instance Ord?
+  x == y = value x == value y && S.fromList (children x) == S.fromList (children y)
 
 ------------------------------------------------------------------------------------------------
 
 -- 3. Цвета (2 балла)
 
 -- | Зададим тип данных RGB, но так как его поля должны быть от 0 до 255, назовем конструктор Unsafe...
---
 data RGB = UnsafeMkRGB
-  { red   :: Int
+  { red :: Int
   , green :: Int
-  , blue  :: Int
-  } deriving (Show)
+  , blue :: Int
+  }
+  deriving (Show, Eq)
 
 -- | ...и зададим новый конструктор, который будет проверять значения полей при инициализации
---
 mkRGB :: Int -> Int -> Int -> Maybe RGB
-mkRGB red green blue
-  | inRange (0, 255) `all` [red, green, blue] = Just $ UnsafeMkRGB red green blue
-  | otherwise                                 = Nothing 
+mkRGB r g b
+  | inRange (0, 255) `all` [r, g, b] = Just $ UnsafeMkRGB r g b
+  | otherwise = Nothing
 
 -- | Аналогично поступим, задавая тип данных CMYK
---
 data CMYK = UnsafeMkCMYK
-  { cyan    :: Int
+  { cyan :: Int
   , magenta :: Int
-  , yellow  :: Int
-  , black   :: Int
-  } deriving (Show)
+  , yellow :: Int
+  , black :: Int
+  }
+  deriving (Show, Eq)
 
 mkCMYK :: Int -> Int -> Int -> Int -> Maybe CMYK
 mkCMYK cyan magenta yellow black
   | inRange (0, 100) `all` [cyan, magenta, yellow, black] = Just $ UnsafeMkCMYK cyan magenta yellow black
-  | otherwise                                             = Nothing 
+  | otherwise = Nothing
+
+unpack :: Maybe a -> a
+unpack (Just x) = x
 
 ---------------------------------------
 
 -- 3.a Напишите инстансы класса ToCMYK для [Int] и для RGB (0,75 балла)
 
 class ToCMYK a where
-    toCMYK :: a -> Maybe CMYK
+  toCMYK :: a -> Maybe CMYK
+
+instance ToCMYK [Int] where
+  toCMYK :: [Int] -> Maybe CMYK
+  toCMYK [c, m, y, k]
+    | inRange (0, 100) `all` [c, m, y, k] = Just $ UnsafeMkCMYK c m y k
+    | otherwise = Nothing
+  toCMYK _ = Nothing
+
+instance ToCMYK RGB where
+  toCMYK :: RGB -> Maybe CMYK
+  toCMYK x =
+    let r' = fromIntegral (red x) / 255
+        g' = fromIntegral (green x) / 255
+        b' = fromIntegral (blue x) / 255
+        k' = 1 - maximum [r', g', b']
+        c = if k == 1 then 0 else round $ (1 - r' - k') / (1 - k') * 100
+        m = if k == 1 then 0 else round $ (1 - g' - k') / (1 - k') * 100
+        y = if k == 1 then 0 else round $ (1 - b' - k') / (1 - k') * 100
+        k = round $ k' * 100
+     in mkCMYK c m y k
 
 ---------------------------------------
 
 -- 3.b Классы типов -- это функции (1 балл)
 
 -- | задайте класс типов ToCMYK используя data (аналогично Eq' из практики)
---
-data DToCMYK = Placeholder
+data DToCMYK a = DMkCMYK
+  { toCMYK' :: a -> Maybe CMYK
+  }
+
+dToCMYKfromList :: DToCMYK [Int]
+dToCMYKfromList = DMkCMYK toCMYKfromList
+ where
+  toCMYKfromList :: [Int] -> Maybe CMYK
+  toCMYKfromList [c, m, y, k]
+    | inRange (0, 100) `all` [c, m, y, k] = Just $ UnsafeMkCMYK c m y k
+    | otherwise = Nothing
+  toCMYKfromList _ = Nothing
 
 -- | реализуйте класс типов DToCMYK для [Int] и для RGB в виде функций (аналогично dEqBool из практики)
+dToCMYKfromRGB :: DToCMYK RGB
+dToCMYKfromRGB = DMkCMYK toCMYKfromRGB
+ where
+  toCMYKfromRGB :: RGB -> Maybe CMYK
+  toCMYKfromRGB x =
+    let r' = fromIntegral (red x) / 255
+        g' = fromIntegral (green x) / 255
+        b' = fromIntegral (blue x) / 255
+        k' = 1 - maximum [r', g', b']
+        c = if k == 1 then 0 else round $ (1 - r' - k') / (1 - k') * 100
+        m = if k == 1 then 0 else round $ (1 - g' - k') / (1 - k') * 100
+        y = if k == 1 then 0 else round $ (1 - b' - k') / (1 - k') * 100
+        k = round $ k' * 100
+     in mkCMYK c m y k
 
 ---------------------------------------
 
 -- 3.c Используйте инстансы (0,25 балла)
 --     Приведите пример использования инстансов [Int] и RGB реализованных в 3a и 3b (должно получится 4 примера)
 
-------------------------------------------------------------------------------------------------
+-- В тестах
 
 -- 4. Сделайте функцию `pointful` бесточечной, объясняя каждый шаг по примеру из практики
 --    (1,5 балла)
 
+pointful :: (a -> b -> c) -> a -> (d -> b) -> d -> c
 pointful a b c d = a b (c d)
+
+-- pointful a b c d = a b $ c d            -- минус скобки
+-- pointful a b c   = a b $ c              -- eta-reduction
+-- pointful a b c   = (a b) $ c            -- частичное применение a
+-- pointful a b c   = (a b) . c            -- композиция
+-- pointful a b c   = (.) (a b) c          -- . is a usual function
+-- pointful a b c   = ((.) (a b)) $ c      -- частичное применение (.)
+-- pointful a b     = (.) (a b)            -- eta-reduction
+-- pointful a b     = (.) $ a b            -- частичное применение (.)
+-- pointful a b     = (.) . a $ b          -- композиция
+-- pointful a       = (.) . a              -- eta-reduction
+-- pointful a       = (.) (.) a            -- . is a usual function
+-- pointful a       = ((.) .) $ a          -- частичное применение (.)
+-- pointless        = ((.) .)              -- eta-reduction
+
+pointless :: (a -> b -> c) -> a -> (d -> b) -> d -> c
+pointless = ((.) .)
 
 ------------------------------------------------------------------------------------------------
 
@@ -131,26 +315,53 @@ pointful a b c d = a b (c d)
 --     и реализуйте для него класс типов `Enum`
 --     https://hackage.haskell.org/package/base-4.19.0.0/docs/Prelude.html#t:Enum
 
-data Day = MyDaysAreHere
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
+  deriving (Show, Eq)
+
+instance Enum Day where
+  toEnum :: Int -> Day
+  toEnum n = case n `mod` 7 of
+    0 -> Monday
+    1 -> Tuesday
+    2 -> Wednesday
+    3 -> Thursday
+    4 -> Friday
+    5 -> Saturday
+    6 -> Sunday
+
+  fromEnum :: Day -> Int
+  fromEnum day = case day of
+    Monday -> 0
+    Tuesday -> 1
+    Wednesday -> 2
+    Thursday -> 3
+    Friday -> 4
+    Saturday -> 5
+    Sunday -> 6
+
+  succ :: Day -> Day
+  succ day = toEnum $ fromEnum day + 1
+
+  pred :: Day -> Day
+  pred day = toEnum $ fromEnum day - 1
 
 ---------------------------------------
 
 -- 5.b Реализуйте следующие функции
 
 -- | Возвращает следующий день
---
 nextDay :: Day -> Day
-nextDay = undefined
+nextDay = succ
 
 -- | Возвращает предыдущий день
---
 dayBefore :: Day -> Day
-dayBefore = undefined
+dayBefore = pred
 
 -- | Возвращает количество от текущего до ближайшей субботы
---
 daysBeforeWeekend :: Day -> Int
-daysBeforeWeekend = undefined
+daysBeforeWeekend day
+  | fromEnum day <= 5 = 5 - fromEnum day -- если сегодня суббота то сегодня ближ суббота
+  | otherwise = 6
 
 ------------------------------------------------------------------------------------------------
 
@@ -169,32 +380,80 @@ data List a = Nil | Cons a (List a)
 
 instance Functor List where
   fmap :: (a -> b) -> List a -> List b
-  fmap = undefined
+  fmap _ Nil = Nil
+  fmap f (Cons x xs) = Cons (f x) (fmap f xs)
 
 ---------------------------------------
 
 -- 6.b Реализуйте инстанс Functor для дерева из задания 2 (0,5 балла)
+
+instance Functor Tree where
+  fmap :: (a -> b) -> Tree a -> Tree b
+  fmap f (Node v []) = Node (f v) []
+  fmap f (Node v cs) = Node (f v) (L.map (fmap f) cs)
 
 ---------------------------------------
 
 -- 6.c(*) Реализуйте инстанс Functor для пары (1 балл)
 
 data Pair a b = Pair a b
-  deriving (Show)
+  deriving (Show, Eq)
+
+instance Functor (Pair a) where
+  fmap :: (b -> c) -> Pair a b -> Pair a c
+  fmap f (Pair x y) = Pair x (f y)
 
 -- С какими трудностями вы столкнулись?
 
+-- Там в примерах по ссылке было решение).
+-- Видимо это для того, чтобы выполнялись условия
+--
+-- Identity
+--   fmap id == id
+-- Composition
+--   fmap (f . g) == fmap f . fmap g
+--
+-- для tuple. Например: (a, b, c) = ((a, b), c)
+--
+-- fmap (f . g) ((a, b), c) = (a, b, f (g c))
+--
+-- fmap f . fmap g $ ((a, b), c) =
+--      = fmap f ((a, b), g c) =
+--         = ((a, b), f (g c))
+--
 ------------------------------------------------------------------------------------------------
 
 -- 7. Класс типов `Bifunctor` (0,5 балла)
 
--- Вы реализовывали функцию mapEither -- она аналочна функции bimap из Bifunctor
+-- Вы реализовывали функцию mapEither -- она аналогична функции bimap из Bifunctor
 -- https://hackage.haskell.org/package/base-4.19.0.0/docs/Data-Bifunctor.html#t:Bifunctor
 
--- Реализуйте инстанс Functor для Either и пары
+-- Реализуйте инстанс Bifunctor для Either и пары
+
+data Either' a b = Left' a | Right' b
+  deriving (Show, Eq)
+
+instance Bifunctor Either' where
+  bimap :: (a -> c) -> (b -> d) -> Either' a b -> Either' c d
+  bimap f _ (Left' x) = Left' (f x)
+  bimap _ g (Right' y) = Right' (g y)
+
+instance Bifunctor Pair where
+  bimap :: (a -> c) -> (b -> d) -> Pair a b -> Pair c d
+  bimap f g (Pair x y) = Pair (f x) (g y)
 
 ------------------------------------------------------------------------------------------------
 
 -- 8. Задайте свой класс типов, опираясь на интересы, хобби и фазу Луны (0,25 балла)
 
 ------------------------------------------------------------------------------------------------
+
+data Moba = Dota | Lol | Hots
+  deriving (Show)
+
+class MobaPlayer a where
+  play :: a -> String
+
+instance MobaPlayer Moba where
+  play :: Moba -> String
+  play _ = "Loser"
--- hw3/test/TestSpec.hs
+++ hw3/test/TestSpec.hs
@@ -1,19 +1,144 @@
 module TestSpec (spec) where
-    
-import Test.Hspec
-  (
-    Spec
-  , it
-  , shouldBe
-  , describe
-  )
 
 import MyLib
 
+import Data.Bifunctor (Bifunctor (bimap))
+import Test.Hspec (
+  Spec,
+  describe,
+  it,
+  shouldBe,
+ )
+
+default (Int, Double)
 
 spec :: Spec
 spec = do
-  describe "traceFoldl" $ do
+  describe "ChurchNumber" $ do
     it "returns the correct result" $ do
-      traceFoldl (+) 0 [1, 2, 3] `shouldBe` (6 :: Int)
-      traceFoldl (*) 1 [1, 2, 3, 4] `shouldBe` (24 :: Int)
+      let x = cn 3
+      let y = cn 5
+      x == Succ (Succ (Succ Zero)) `shouldBe` True
+      y == Succ (Succ (Succ (Succ (Succ Zero)))) `shouldBe` True
+      x + y `shouldBe` cn 8
+      cn 11513 + cn 8795755 `shouldBe` cn 8807268
+      x - y `shouldBe` cn 0
+      x * y `shouldBe` cn 15
+      cn 65 * cn 43 `shouldBe` cn 2795
+      x == y `shouldBe` False
+      x < y `shouldBe` True
+      x > y `shouldBe` False
+      x <= y `shouldBe` True
+      x >= y `shouldBe` False
+  -- Variable not in scope. Почему? Я не понимаю...
+  -- range (cn 1, cn 3) `shouldBe` [cn 1, cn 2, cn 3]
+  -- inRange (cn 1, cn 3) (cn 2) `shouldBe` True
+  -- inRange (cn 1, cn 3) (cn 4) `shouldBe` False
+  -- index (cn 1, cn 3) (cn 2) `shouldBe` 1
+  -- index (cn 1, cn 100000) (cn 10000) `shouldBe` 9999
+  -- index (cn 1, cn 1000) (cn 40000) `shouldBe` -1
+
+  describe "Tree" $ do
+    it "returns the correct result" $ do
+      let tree1 = Node "F" [Node "B" [Node "A" [], Node "D" [Node "C" [], Node "E" []]], Node "G" [Node "I" [Node "H" []]]]
+      -- something like this
+      --      F
+      --   /    \
+      --  B      G
+      -- / \      \
+      -- A  D      I
+      --   / \      \
+      --   C E       H
+      let tree11 = Node "F" [Node "B" [Node "A" [], Node "D" [Node "E" [], Node "C" []]], Node "G" [Node "I" [Node "H" []]]]
+      -- something like this
+      --      F
+      --   /    \
+      --  B      G
+      -- / \      \
+      -- A  D      I
+      --   / \      \
+      --   E C       H
+      let tree12 = Node "F" [Node "G" [Node "I" [Node "H" []]], Node "B" [Node "A" [], Node "D" [Node "E" [], Node "C" []]]]
+      -- something like this
+      --      F
+      --   /    \
+      --  G      B
+      -- /      / \
+      -- I     A   D
+      --  \       / \
+      --   H     E   C
+      let tree2 = Node "F" [Node "B" [Node "A" [], Node "D" [Node "C" [], Node "E" []]], Node "G" []]
+      -- something like this
+      --      F
+      --   /    \
+      --  B      G
+      -- / \
+      -- A  D
+      --   / \
+      --   C E
+      let tree3 = Node "B" [Node "A" [], Node "C" []]
+      -- something like this
+      --  B
+      -- / \
+      -- A  C
+      let tree31 = Node "B" [Node "C" [], Node "A" []]
+      -- something like this
+      --  B
+      -- / \
+      -- C  A
+      let tree4 = Node "B" []
+      -- something like this
+      -- B
+      show (Pre tree1) `shouldBe` "[F,B,A,D,C,E,G,I,H]"
+      show (Pre tree3) `shouldBe` "[B,A,C]"
+      show (Pre tree4) `shouldBe` "[B]"
+      show (In tree1) `shouldBe` "[A,B,C,D,E,F,H,I,G]"
+      show (In tree3) `shouldBe` "[A,B,C]"
+      show (In tree4) `shouldBe` "[B]"
+      show (Post tree1) `shouldBe` "[A,C,E,D,B,H,I,G,F]"
+      show (Post tree3) `shouldBe` "[A,C,B]"
+      show (Post tree4) `shouldBe` "[B]"
+      tree1 == tree11 `shouldBe` True
+      tree1 == tree12 `shouldBe` True
+      tree1 == tree2 `shouldBe` False
+      tree1 == tree3 `shouldBe` False
+      tree1 == tree4 `shouldBe` False
+      tree3 == tree31 `shouldBe` True
+
+  describe "CMYK" $ do
+    it "returns the correct result" $ do
+      -- Есть ли способ не писать :: Int каждый раз?
+      toCMYK [0 :: Int, 0 :: Int, 0 :: Int, 0 :: Int] `shouldBe` Just (UnsafeMkCMYK 0 0 0 0)
+      toCMYK [100 :: Int, 100 :: Int, 100 :: Int, 100 :: Int] `shouldBe` Just (UnsafeMkCMYK 100 100 100 100)
+      toCMYK [0 :: Int, 0 :: Int, 0 :: Int, 101 :: Int] `shouldBe` Nothing
+      toCMYK [0 :: Int, 0 :: Int, 0 :: Int] `shouldBe` Nothing
+      toCMYK [0 :: Int, 0 :: Int, 0 :: Int, 0 :: Int, 0 :: Int] `shouldBe` Nothing
+      toCMYK [0 :: Int, 0 :: Int, 0 :: Int, -1 :: Int] `shouldBe` Nothing
+      (toCMYK . unpack) (mkRGB 0 0 0) `shouldBe` Just (UnsafeMkCMYK 0 0 0 100)
+      -- ... but got: Nothing ??????????
+      -- ghci> (toCMYK . unpack) (mkRGB 0 0 0) == Just (UnsafeMkCMYK 0 0 0 100)
+      -- True
+      (toCMYK . unpack) (mkRGB 100 43 123) `shouldBe` Just (UnsafeMkCMYK 19 65 0 52)
+
+  describe "DAY" $ do
+    it "returns the correct result" $ do
+      nextDay Monday `shouldBe` Tuesday
+      nextDay Sunday `shouldBe` Monday
+      dayBefore Monday `shouldBe` Sunday
+      dayBefore Sunday `shouldBe` Saturday
+      daysBeforeWeekend Sunday `shouldBe` 6
+      daysBeforeWeekend Saturday `shouldBe` 0
+      daysBeforeWeekend Monday `shouldBe` 5
+
+  describe "Functor" $ do
+    it "returns the correct result" $ do
+      fmap (+ 1) [1 :: Int, 2 :: Int, 3 :: Int, 4 :: Int] `shouldBe` [2, 3, 4, 5]
+      fmap (+ 1) (Pair (1 :: Int) (2 :: Int)) `shouldBe` Pair (1 :: Int) (3 :: Int)
+      fmap (++ "A") (Node "A" [Node "B" [], Node "C" []]) `shouldBe` Node "AA" [Node "BA" [], Node "CA" []]
+
+  describe "Bifunctor" $ do
+    it "returns the correct result" $ do
+      bimap (+ 1) (+ 1) (Pair (1 :: Int) (2 :: Int)) `shouldBe` Pair (2 :: Int) (3 :: Int)
+      bimap (++ "A") (+ 1) (Pair "A" (2 :: Int)) `shouldBe` Pair "AA" (3 :: Int)
+      bimap (+ 1) (++ "A") (Left' (1 :: Int)) `shouldBe` Left' (2 :: Int)
+      bimap even (++ "A") (Right' "A") `shouldBe` Right' "AA"
