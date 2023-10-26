8065cf3c15e98327f98c14896edc1bf7e21075b5
jmkostina <113294009+jmkostina@users.noreply.github.com>
2023-10-26 20:31:10 +0300

    Update TestSpec

--- hw03/test/TestSpec.hs
+++ hw03/test/TestSpec.hs
@@ -13,8 +13,6 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
-  -- lhint  дал замечаниz к скобкам для compare, но они нужны, потому что иначе 
-  -- во время тестирования компилятор не понимает, к чему относится Succ без скобок
     describe "ChurchNumbers, Eq" $ do
       it "Two Zeros" $ do
         Zero == Zero `shouldBe` True
@@ -30,27 +28,63 @@ spec = do
         compare Zero Zero `shouldBe` EQ
 
       it "Zero and Succ" $ do
-        compare Zero (Succ (Zero)) `shouldBe` LT
+        compare Zero (Succ Zero) `shouldBe` LT
 
       it "Succ and Zero" $ do
-        compare (Succ (Succ (Zero))) Zero `shouldBe` GT
+        compare (Succ (Succ Zero)) Zero `shouldBe` GT
 
       it "Two Succ" $ do
-        compare (Succ (Succ (Zero))) (Succ (Zero)) `shouldBe` GT
+        compare (Succ (Succ Zero)) (Succ Zero) `shouldBe` GT
 
     describe "ChurchNumbers, Num" $ do
       it "Zero + Succ" $ do
-        (Zero + Succ (Succ (Zero))) `shouldBe` Succ (Succ (Zero))
+        (Zero + Succ (Succ Zero)) `shouldBe` Succ (Succ Zero)
 
       it "Succ - Zero" $ do
         (Succ (Succ Zero) - Zero) `shouldBe` Succ (Succ Zero)
 
       it "Succ * Succ" $ do
-        (Succ (Succ Zero) * Succ (Succ Zero)) `shouldBe` Succ (Succ (Succ (Succ (Zero))))
+        (Succ (Succ Zero) * Succ (Succ Zero)) `shouldBe` Succ (Succ (Succ (Succ Zero)))
 
-    -- describe "Trees"...
+      it "abs four" $ do
+        abs (Succ (Succ (Succ (Succ Zero)))) `shouldBe` Succ (Succ (Succ (Succ Zero)))
 
-    -- describe "Colors"...
+      it "signum Zero" $ do
+        signum Zero `shouldBe` Zero
+      
+      it "signum of four" $ do
+        signum (Succ (Succ (Succ (Succ Zero)))) `shouldBe` Succ Zero
+
+    describe "Trees traversal" $ do
+      it "In-Order" $ do
+        let tree = In (Node (1 :: Int) [Node (2 :: Int) [], Node (3 :: Int) [Node (4 :: Int) [], Node (5 :: Int) []]])
+        show tree `shouldBe` "24531"
+
+      it "Pre-Order" $ do
+        let tree = Pre (Node (1 :: Int) [Node (2 :: Int) [], Node (3 :: Int) [Node (4 :: Int) [], Node (5 :: Int) []]])
+        show tree `shouldBe` "12345"
+
+      it "Post-Order" $ do
+        let tree = Post (Node (1 :: Int) [Node (2 :: Int) [], Node (3 :: Int) [Node (4 :: Int) [], Node (5 :: Int) []]])
+        show tree `shouldBe` "24531"
+
+    describe "Eq Tree" $ do
+      let myTree1 = Node (1 :: Int) [Node (2 :: Int) [], Node (3 :: Int) []]
+      let myTree2 = Node (4 :: Int) [Node (5 :: Int) [], Node (7 :: Int) [Node (6 :: Int) []]]
+      let myTree3 = Node (4 :: Int) [Node (5 :: Int) [], Node (6 :: Int) [Node (7 :: Int) []]]
+      let myTree4 = myTree1
+
+      it "Different size" $ do
+        myTree1 == myTree2 `shouldBe` False
+
+      it "Different order" $ do
+        myTree2 == myTree3 `shouldBe` False
+      
+      it "Eq trees" $ do
+        myTree1 == myTree4 `shouldBe` True
+
+      it "Same tree" $ do
+        myTree3 == myTree3 `shouldBe` True
 
     describe "Next Day" $ do
       it "Tuesday -> Wednesday" $ do
