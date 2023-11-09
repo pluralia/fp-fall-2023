76fc1e5b97724b5784e9a28faf1b49835cb0f9f4
Denis Tereshchenko <dotereschenko@edu.hse.ru>
2023-11-09 22:13:53 +0300

    complete hw1

--- hw1.hs
+++ hw1.hs
@@ -4,13 +4,42 @@ testFib1 :: Bool
 testFib1 = fib 8 == 21
 
 testFib2 :: Bool
-testFib2 = undefined
+testFib2 = fibTail 1000 == 817770325994397771
+
+testOper1 :: Bool
+testOper1 = myAnd True True == True
+
+testLength :: Bool
+testLength = myLength [1,2,3] == 3
+
+testTail :: Bool
+testTail = myTail [1, 2, 3] == Just [2, 3]
+
+testInit :: Bool
+testInit = myInit [1,2,4,5] == Just [1,2,4]
+
+testAppend :: Bool
+testAppend = myAppend [1,2,3,4,5] [] == [1,2,3,4,5]
+
+testReverse :: Bool
+testReverse = myReverse [1,2,3,4] == [4,3,2,1]
+
+testByIndex :: Bool
+testByIndex = elemByIndex [1,2,3,4] 1 == Just 2
+
 
 -- | Вызвать из все разом можно так:
 testAll :: Bool
 testAll = and [
     testFib1
   , testFib2
+  , testOper1
+  , testLength
+  , testTail
+  , testInit
+  , testAppend
+  , testReverse
+  , testByIndex
   ]
 
 -------------------------------------------------------------------------------
@@ -18,7 +47,12 @@ testAll = and [
 -- | 1. Реализуйте логическое И как функцию и как оператор (выберите произвольные символы для вашего оператора).
 --
 myAnd :: Bool -> Bool -> Bool
-myAnd = undefined
+myAnd True True = True
+myAnd _ _ = False
+
+infix 3 &&&
+(&&&) :: Bool -> Bool -> Bool
+a &&& b = a && b
 
 -------------------------------------------------------------------------------
 
@@ -27,15 +61,20 @@ myAnd = undefined
 -- | без хвостовой рекурсии
 --
 fib :: Int -> Int
-fib = undefined
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
 
 -- | c хвостовой рекурсией
 --
 fibTail :: Int -> Int
-fibTail = undefined
-
+fibTail n = go n (0,1)
+  where
+    go !n (!a, !b) | n==0      = a
+                   | otherwise = go (n-1) (b, a+b)
 -- Позапускайте обе функции на больших числах (> 1000). Какой результат вы получили и почему?
-
+-- Хвостовая реккурсия значительно быстрее работает при вычислении большого числа фиб.
+--  количество элементов накапливается в стек. Хвостовую реккурсию компилятор переделывает в цикл и выдает последний результат обращения к вспом. фун.
 -------------------------------------------------------------------------------
 
 -- 3. Повторяем каррирование. Реализуйте 2 функции.
@@ -43,12 +82,11 @@ fibTail = undefined
 -- | превращает некаррированную функцию в каррированную
 --
 myCurry :: ((a, b) -> c) -> a -> b -> c
-myCurry = undefined
+myCurry a b c = a (b, c)
 
 -- | превращает каррированную функцию в некаррированную
---
 myUncurry :: (a -> b -> c) -> (a, b) -> c
-myUncurry = undefined
+myUncurry  a (b, c) = a b c
 
 -------------------------------------------------------------------------------
 
@@ -59,40 +97,67 @@ myUncurry = undefined
 
 -- | возвращает длину списка
 --
-myLength = undefined
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (_:xs) = 1 + myLength xs
 
 -- | возвращает хвост списка
 --
-myTail = undefined
+myTail :: [a] -> Maybe [a]
+myTail [] = Nothing
+myTail (_:xs) = Just xs
 
 -- | возвращает список без последнего элемента
 --
-myInit = undefined
-
+myInit::[a]-> Maybe [a]
+myInit [] = Nothing
+myInit [_] = Just []
+myInit (x:xs) = case myInit xs of
+  Nothing -> Nothing
+  Just y -> Just (x : y)
+  
 -- | объединяет 2 списка
 --
-myAppend = undefined
+myAppend :: [a] -> [a] -> [a]
+myAppend [] join = join
+myAppend (x:xs) join = x : myAppend xs join
 
 -- | разворачивает список
 --
-myReverse = undefined
+myReverse :: [a] ->  [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
 
 -- | выдаёт элемент списка по индексу
 --
-elemByIndex = undefined
+elemByIndex :: [a] -> Int -> Maybe a
+elemByIndex [] _ = Nothing
+elemByIndex _ 0 = Nothing
+elemByIndex (x : _) 1 = Just x
+elemByIndex (x : xs) n
+  | n < 0 = Nothing
+  | otherwise = elemByIndex xs (n - 1)
+
+
+
+
+
 
 -------------------------------------------------------------------------------
 
 -- 5. Реализуйте map для разных типов.
 
 mapEither :: (a -> c) -> (b -> d) -> Either a b -> Either c d 
-mapEither = undefined
+mapEither f _ (Left a) = Left (f a)
+mapEither _ g (Right b) = Right (g b)
 
 mapMaybe :: (a -> b) -> Maybe a -> Maybe b
-mapMaybe = undefined
+mapMaybe _ Nothing = Nothing
+mapMaybe f (Just a) = Just (f a)
 
 mapList :: (a -> b) -> [a] -> [b]
-mapList = undefined
+mapList _ [] = []
+mapList f (x:xs) = f x : mapList f xs
 
 -------------------------------------------------------------------------------
 
@@ -101,33 +166,48 @@ mapList = undefined
 data ChurchNumber = Zero | Succ ChurchNumber
   deriving (Show, Eq)
 
--- | Видно, что, например, число три, закодированное с помощью типа 'ChurchNumber',
---   является трёхкратным применением конструктора данных 'Succ' (который тоже функция) к 'Zero'
---
-churchThree :: ChurchNumber
-churchThree = Succ (Succ (Succ Zero))
+chSucc :: ChurchNumber -> ChurchNumber
+chSucc n = Succ n
 
--- Реализуйте функции `succ`, `add`, `mult`, `pow` и `prev` для типа 'ChurchNumber'
---    - укажите типы функций
---    - напишите тесты
+chAdd :: ChurchNumber -> ChurchNumber -> ChurchNumber
+chAdd m n = case m of
+  Zero   -> n
+  Succ x -> chSucc (chAdd x n)
 
-chSucc = undefined
+chMult :: ChurchNumber -> ChurchNumber -> ChurchNumber
+chMult m n = case m of
+  Zero   -> Zero
+  Succ x -> chAdd n (chMult x n)
 
-chAdd = undefined
+chPow :: ChurchNumber -> ChurchNumber -> ChurchNumber
+chPow m n = case n of
+  Zero   -> chSucc Zero
+  Succ x -> chMult m (chPow m x)
 
-chMult = undefined
+chPrev :: ChurchNumber -> ChurchNumber
+chPrev n = case n of
+  Zero       -> Zero
+  Succ Zero  -> Zero
+  Succ x     -> x
 
-chPow = undefined
+-- Тесты
+churchOne, churchTwo, churchThree :: ChurchNumber
+churchOne = Succ Zero
+churchTwo = Succ churchOne
+churchThree = Succ churchTwo
 
-chPrev = undefined
-
--- | Тесты могут выглядеть так
---
-testChSucc :: Bool
-testChSucc = chSucc churchTwo == churchThree
+testChAdd :: Bool
+testChAdd = chAdd churchTwo churchThree == churchFive
   where
-    churchTwo :: ChurchNumber
-    churchTwo = Succ (Succ Zero)
+    churchFive :: ChurchNumber
+    churchFive = Succ (Succ (Succ (Succ (Succ Zero))))
+
+testChMult :: Bool
+testChMult = chMult churchTwo churchThree == churchSix
+  where
+    churchSix :: ChurchNumber
+    churchSix = Succ (Succ (Succ (Succ (Succ (Succ Zero)))))
+
 
 -------------------------------------------------------------------------------
 
@@ -136,11 +216,17 @@ testChSucc = chSucc churchTwo == churchThree
 
 -- | двигает точку на заданное расстояние по каджой из координат
 --
-move = undefined
+data Point  = Point { x :: Float, y :: Float }
+  deriving(Show)
+
+move :: Point -> Float -> Point
+move (Point x1 y1) distance = Point (x1 + distance) (y1 + distance)
+
 
 -- | возвращает дистанцию между 2 точками
 --
-dist = undefined
+dist :: Point -> Point -> Float
+dist (Point x1 y1) (Point x2 y2) = sqrt ((x2 - x1)^2 + (y2 - y1)^2)
 
 -------------------------------------------------------------------------------
 
@@ -188,13 +274,40 @@ binTreeOfStringsCopy = binTreeOfStrings
 --     - раскомментируйте строку с типом, когда объявите `IntTree`
 --
 -- binTreeOfInts :: IntTree
-binTreeOfInts = undefined
+type IntTree = BinaryTree Int
 
 -- Напишите поиск элемента в таком дереве.
 --    - раскомментируйте строку с типом, когда объявите `IntTree`
 --    - напишите тесты
---
--- isPresented :: Int -> IntTree -> Bool
-isPresented = undefined
+binTreeOfInts :: IntTree
+binTreeOfInts = 
+  Node 5 
+    (Node 3 
+      (Node 2 Leaf Leaf) 
+      (Node 4 Leaf Leaf)
+    ) 
+    (Node 8 
+      (Node 7 
+        (Node 6 Leaf Leaf) 
+        (Node 9 Leaf Leaf)
+      ) 
+      Leaf
+    )
 
+isPresented :: Int -> IntTree -> Bool
+isPresented _ Leaf = False
+isPresented target (Node value left right)
+  | target == value = True
+  | target < value  = isPresented target left
+  | otherwise       = isPresented target right
+
+testIsPresented :: Bool
+testIsPresented =
+  and
+    [ isPresented 6 binTreeOfInts 
+    , not (isPresented 10 binTreeOfInts) 
+    , isPresented 5 binTreeOfInts 
+    , isPresented 2 binTreeOfInts  
+    , not (isPresented 1 binTreeOfInts)
+    ]
 -------------------------------------------------------------------------------
