9a9239366217cf858e78e403928c6471b894ea29
Ivan <113239177+paiksonn@users.noreply.github.com>
2023-11-24 13:53:57 +0300

    Update TestSpec.hs

--- test/TestSpec.hs
+++ test/TestSpec.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE FlexibleInstances #-}
 module TestSpec where
 
 import Test.Hspec
@@ -6,13 +5,18 @@ import MyLib
 
 spec :: Spec
 spec = do
+    describe "1. Парсер строки CSV формата with Maybe" $ do
+        it "My test CSV" $ do
+            let col = ["Year", "Make", "Model"]
+            testParserIO "test.csv" (rowP col) `shouldReturn` True
+
+
     describe "2. Парсер FASTA" $ do
         it "My test FASTA" $ do
             let testFilePath1 = "D:\\Studies_at_HSE_spb\\3 semestr\\HASKELL\\homeworks\\hw7\\test.fasta"
-            testParserIO testFilePath1 fastaListP `shouldReturn` True
-
-    -- дает False и я не знаю почему, у меня закончились идеи
-    describe "3. Парсер PDB" $ do  
+            testMYParserIO testFilePath1 fastaListP `shouldReturn` True
+        
+    describe "3. Парсер PDB" $ do 
         it "My test only_atoms" $ do
             let testFilePath2 = "D:\\Studies_at_HSE_spb\\3 semestr\\HASKELL\\homeworks\\hw7\\only_atoms.pdb"
             testParserIO testFilePath2 pdbParser' `shouldReturn` True
