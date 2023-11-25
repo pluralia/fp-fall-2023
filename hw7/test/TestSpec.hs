2da22d8abde7a80e75a4bdf3b11227b3ad532db4
Artem Levonovich <113240536+Artem9999999999999999999999@users.noreply.github.com>
2023-11-25 15:14:38 +0300

    Update TestSpec.hs

--- hw7/test/TestSpec.hs
+++ hw7/test/TestSpec.hs
@@ -1,7 +1,8 @@
 module TestSpec (spec) where
 
 import MyLib
-
+import Data.Map.Strict     (fromList)
+import Parser
 
 import Test.Hspec
   (
@@ -9,17 +10,25 @@ import Test.Hspec
   , it
   , describe
   , shouldReturn
+  , shouldBe
   )
 
 spec :: Spec
 spec = do
     describe "Tests" $ do
-      it "FASTA tests" $ do
-        testParserIO "src/test.fasta" fastaListP     `shouldReturn` (True :: Bool)
+      it "rowP tests" $ do
+        runParser (rowP ["Column1", "Column2", "Column3"]) "123, 456, 789" `shouldBe` Just (Row (fromList [("Column1",Just (IntValue 123)),("Column2",Just (IntValue 456)),("Column3",Just (IntValue 789))]),"")
+        runParser (rowP ["Name", "Age", "Score"]) "Bob, 25, 85.5"          `shouldBe` Just (Row (fromList [("Age",Just (IntValue 25)),("Name",Just (StringValue "Bob")),("Score",Just (FloatValue 85.5))]),"")
 
+      it "FASTA tests" $ do
+        testParserIO "src/test.fasta" fastaListP      `shouldReturn` (True :: Bool)
+        testFullyParsedIO "src/test.fasta" fastaListP `shouldReturn` (True :: Bool)
+      
       it "test pdbP" $ do
-        testParserIO "src/only_atoms.pdb"       pdbP `shouldReturn` (True :: Bool)
-        testParserIO "src/atoms_with_bonds.pdb" pdbP `shouldReturn` (True :: Bool)
+        testParserIO "src/only_atoms.pdb"       pdbP      `shouldReturn` (True :: Bool)
+        testParserIO "src/atoms_with_bonds.pdb" pdbP      `shouldReturn` (True :: Bool)
+        testFullyParsedIO "src/only_atoms.pdb"       pdbP `shouldReturn` (True :: Bool)
+        testFullyParsedIO "src/atoms_with_bonds.pdb" pdbP `shouldReturn` (True :: Bool)
 
         
         
