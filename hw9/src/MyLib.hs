3661e016bb9055cfcf28526df687fd4dbf81036d
Artem Levonovich <113240536+Artem9999999999999999999999@users.noreply.github.com>
2023-11-29 18:39:13 +0300

    Add files via upload

---
+++ hw9/src/MyLib.hs
@@ -0,0 +1,475 @@
+{-# LANGUAGE LambdaCase #-} --hlint посоветовала
+
+module MyLib where
+
+{- cabal:
+    build-depends: base, mtl, containers
+-}
+{- install for ghci:
+    > cabal install mtl
+    > ghci
+    >> :set -package mtl
+-}
+
+import           Control.Monad.Writer.Lazy
+import           Control.Monad.Reader
+import           Control.Monad.State.Lazy
+import           Control.Monad()
+import qualified Data.Map.Strict as M
+import qualified System.Random as R -- cabal install --lib  random
+
+-------------------------------------------------------------------------------
+
+-- 1. Writer: Брандмауэр (3 балла)
+
+--    Напишите очень простой брандмауэр: он фильтрует пакеты на основе базы правил,
+--    соответствующих IP-адресам отправителя и получателя.
+--    Основная задача брандмауэра -- фильтрация пакетов, но мы хотели бы, чтобы он также вел
+--    журнал своей работы (логи)
+
+-- | Формат логов
+--
+data Entry = Log {
+      count :: Int     -- количество таких сообщений подряд, идущих друг за другом
+    , msg   :: String  -- сообщение
+    } deriving (Show, Eq)
+
+-- | Добавляет сообщение в лог
+--
+logMsg :: String -> Writer [Entry] ()
+logMsg s = tell [Log 1 s]
+
+-- | Задайте тип, выражающий IP-адрес
+--
+newtype IP = IPAddress String
+  deriving (Show, Eq)
+
+data Action = Accept | Reject
+  deriving (Show, Eq)
+
+data Rule = Rule {
+      action' :: Action
+    , source :: IP
+    , destination :: IP
+    } deriving (Show, Eq)
+
+data Packet = Packet {
+      pSource :: IP
+    , pDestination :: IP
+    } deriving (Show, Eq)
+
+-- | Возвращает первое правило, действующее на пакет
+--
+match :: [Rule] -> Packet -> Maybe Rule
+match []     _      = Nothing
+match (r:rs) packet =
+  if source r == pSource packet && destination r == pDestination packet
+    then Just r
+    else match rs packet
+
+
+-- | Фильтрует 1 пакет
+-- 
+filterOne :: [Rule] -> Packet -> Writer [Entry] (Maybe Packet)
+filterOne rules packet = do
+  case match rules packet of
+    Just rule -> do
+      logMsg $ "Packet accepted: " ++ show packet
+      return $ if action' rule == Accept then Just packet else Nothing
+    Nothing -> do
+      logMsg $ "Packet rejected: " ++ show packet
+      return Nothing
+
+-- Немного усложним задачу: теперь мы хотим объединить дублирующиеся последовательные записи в журнале.
+-- Ни одна из существующих функций не позволяет изменять результаты предыдущих этапов вычислений,
+-- но мы можем использовать прием "отложенного протоколирования", чтобы добавлять запись в лог только
+-- после получения новой записи, которая не совпадает с предыдущими.
+
+-- | Объединяет одинаковые записи в конце лога
+--   Эта функция использует [Entry] как в качестве типа лога, так и в качестве типа результата.
+--   При объединении двух одинаковых сообщений результатом будет только сообщение с увеличенным счетчиком.
+--   При объединении двух разных сообщений, в лог записывается первое сообщение, а второе возвращается в качестве результата.
+--
+mergeEntries :: [Entry] -> [Entry] -> Writer [Entry] [Entry]
+mergeEntries [] ys = tell ys >> return []
+mergeEntries xs [] = tell xs >> return []
+mergeEntries (log1:rest1) (log2:rest2)
+  | msg log1 == msg log2 =
+    let merged = Log (count log1 + count log2) (msg log1)
+    in mergeEntries rest1 rest2 >>= \mergedRest ->
+        tell [merged] >> return mergedRest
+  | otherwise =
+    mergeEntries rest1 (log2 : rest2) >>= \rest ->
+      tell [log1] >> return rest
+
+
+-- | Применяет входную функцию к списку значений, чтобы получить список Writer.
+--   Затем запускает каждый Writer и объединяет результаты.
+--   Результатом работы функции является Writer, значение которого -- список всех значений из всех Writer,
+--   а лог -- результат слияния всех логов всех Writer
+--
+-- 'initial' -- изначальное значение лога
+--
+groupSame :: (Monoid a) => a -> (a -> a -> Writer a a) -> [b] -> (b -> Writer a c) -> Writer a [c]
+groupSame initial merge xs fn =
+  let go [] = return []
+      go (x:xs') = do
+        (result, log') <- listen (fn x)
+        mergedLog <- merge initial log'
+        rest <- go xs'
+        tell mergedLog
+        return (result : rest)
+  in go xs
+
+-- Пример использования groupSame
+
+-- Функция для слияния логов, просто конкатенирующая строки
+mergeLogs :: String -> String -> Writer String String
+mergeLogs log1 log2 = tell (log1 ++ log2) >> return log1
+
+-- Функция, преобразующая целое число в Writer с удвоенным значением и строковым логом
+doubleAndLog :: Int -> Writer String Int
+doubleAndLog x = tell ("Doubling " ++ show x) >> return (x * 2)
+
+-- Пример списка значений
+values :: [Int]
+values = [1, 2, 3, 4, 5]
+
+-- Использование groupSame для применения doubleAndLog ко всем элементам списка
+-- с слиянием логов mergeLogs и начальным значением лога "Initial Log:\n"
+resultWriter :: Writer String [Int]
+resultWriter = groupSame "Initial Log:\n" mergeLogs values doubleAndLog
+
+-- | Фильтрует список пакетов и возвращает список отфильтрованных пакетов и логи
+filterAll :: [Rule] -> [Packet] -> Writer [Entry] [Packet]
+filterAll _     []               = return []
+filterAll rules (packet:packets) =
+  filterOne rules packet >>=   
+    \case
+      Just p -> filterAll rules packets >>= \ rest -> return (p : rest)
+      Nothing -> filterAll rules packets
+
+
+-------------------------------------------------------------------------------
+
+-- 2. Reader: Инстанцирование шаблонов (2 балла)
+
+-- Рассмотрим проблему инстанцирования шаблонов, содержащих подстановки переменных и включенные шаблоны.
+-- Используя монаду Reader, мы можем поддерживать окружение/контекст всех известных шаблонов и всех известных переменных.
+-- Когда встречается подстановка переменной, мы можем использовать функцию asks для поиска значения переменной в окружении.
+-- Если в шаблон включены определения новых переменных, мы будем должны изменить окружение, добавив в него эти перменные;
+-- продолжить работу на измененном окружении можно с помощью функции local
+
+data Template = Text String
+              | Var Template
+              | Quote Template
+              | Include Template [Definition]
+              | Compound [Template]
+  deriving (Show, Eq)
+
+data Definition = Definition Template Template
+  deriving (Show, Eq)
+
+-- | Окружение -- это имя шаборна в шаблон и имя переменной в переменную
+--
+data Environment = Env {
+      templs :: M.Map String Template
+    , vars   :: M.Map String String
+    } deriving (Show, Eq)
+
+-- | Ищет переменную в окружении
+--
+lookupVar :: String -> Environment -> Maybe String
+lookupVar var env = M.lookup var (vars env)
+
+-- | Ищет шаблон в окружении
+--
+lookupTemplate :: String -> Environment -> Maybe Template
+lookupTemplate name env = M.lookup name (templs env)
+
+-- | Добавляет новые переменные в окружение
+-- 
+addDefs :: M.Map String String -> Environment -> Environment
+addDefs newDefs env = env { vars = M.union (vars env) newDefs }
+
+-- | Резолвит (подставляет все неизвестные) шаблон в строку
+--
+resolve :: Template -> Reader Environment String
+resolve (Text textContent) = return textContent
+resolve (Var templateVar) = 
+  ask >>= \env ->
+    case templateVar of
+      Text varName ->
+        let maybeValue = lookupVar varName env
+        in case maybeValue of
+          Just value -> return value
+          Nothing    -> return ""
+      _ -> resolve templateVar
+resolve _ = return ""
+
+-- Резолвит (подставляет все неизвестные) шаблон и выдает в качестве ответа
+-- пару (имя шаблона, значение)
+resolveDef :: Definition -> Reader Environment (String, String)
+resolveDef (Definition templateName value) =
+  ask >>= \env ->
+    let resolvedValue = runReader (resolve value) env
+        getTemplateName :: Template -> String
+        getTemplateName (Text name)    = name
+        getTemplateName (Var template) = getTemplateName template
+        getTemplateName _              = ""
+    in return (getTemplateName templateName, resolvedValue)
+
+-------------------------------------------------------------------------------
+
+-- 3. State: Генерация случайного значения кастомного типа (1 балл)
+
+-- Чистый функциональный язык не может обновлять значения на месте (почему?)
+-- Распространенной идиомой для имитации таких вычислений с сохранением состояния является
+-- "прохождение" параметра состояния через последовательность функций
+
+-- Чистый функциональный язык характеризуется отсутствием побочных эффектов и изменяемого состояния.
+-- В чистой функциональности значения являются неизменяемыми, и любые изменения создают новые значения вместо модификации существующих.
+-- Это свойство обеспечивает прозрачность ссылок, делая код более предсказуемым и легким в понимании.
+
+-- Рассмотрим пример
+
+data MyType = MT Int Bool Char Int
+  deriving (Show, Eq)
+
+-- Чтобы запустить: `makeRandomValue (mkStdGen 23)`
+
+makeRandomValue :: R.StdGen -> (MyType, R.StdGen)
+makeRandomValue g =
+    let (n,g1) = R.randomR (1, 100) g
+        (b,g2) = R.random g1
+        (c,g3) = R.randomR ('a', 'z') g2
+        (m,g4) = R.randomR (-n, n) g3
+    in (MT n b c m, g4)
+
+-- Этот подход работает, но такой код сложен в сопровождении, может содержать ошибки и быть грязным (что делает его грязным?)
+-- Монада State скрывает потоковую передачу состояния (как вы понимаете слова "потоковая передача состояния") внутри операции >>=,
+-- делая код проще для написания, чтения и модификации.
+
+-- Мне кажется, что Потоковая передача состояния означает, что состояние передается между функциями в виде параметра.
+-- Вместо того, чтобы изменять состояние на месте, каждая функция принимает текущее состояние и возвращает новое состояние вместе с результатом вычисления.
+-- Это обеспечивает иммутабельность данных и изолирует изменения состояния внутри функциональных вычислений.
+--
+-- Прохождение состояния является подходом, который может привести к нескольким вложенным "колбасным" функциям,
+-- что делает код сложным для понимания и сопровождения. Это усложняет отслеживание состояния и может сделать код менее читаемым.
+-- Также, такой код подвержен ошибкам, связанным с передачей состояния между функциями.
+
+-- | Возвращает случайное значение и обновляет состояние генератора случайных чисел
+--
+getAny :: (R.Random a) => State R.StdGen a
+getAny = state R.random
+
+-- | Аналогична getAny, но генерирует значение в границах
+--
+getOne :: (R.Random a) => (a, a) -> State R.StdGen a
+getOne bounds = state $ R.randomR bounds
+
+-- | Используя монаду State с StdGen в качестве состояния, мы можем генерировать случаные значения
+--   заданного типа, не передавая состояния генератора случайных чисел в коде вручную
+--
+makeRandomValueST :: R.StdGen -> (MyType, R.StdGen)
+makeRandomValueST =
+  runState $
+    getOne (1, 100) >>= \n ->
+      getOne (False, True) >>= \b ->
+        getOne ('a', 'z') >>= \c ->
+          getOne (-n, n) >>= \m ->
+            return (MT n b c m)
+
+-------------------------------------------------------------------------------
+
+-- 4. "Императивное" программирование (2 балла)
+
+---------------------------------------
+
+-- | 4.1. Реализуйте функцию `whileM_`, исполняющую заданное вычисление,
+--        пока первое вычисление возвращает 'True'
+--
+whileM_ :: Monad m => m Bool -> m a -> m ()
+whileM_ condition action = condition >>= \shouldContinue ->
+  when shouldContinue
+    $ action >> whileM_ condition action
+
+-- Здесь whileM_ используется для уменьшения счетчика в монаде State Int до нуля.
+-- example представляет собой цикл, который продолжается, пока счетчик больше нуля.
+-- Вычисления запускаются с начальным состоянием, например 5,
+-- и результат представляет собой пару из значения () (поскольку whileM_ возвращает () в каждой итерации) и конечного значения счетчика, то есть 0.
+
+type CounterMonad = State Int
+
+decrementCounter :: CounterMonad ()
+decrementCounter = modify (\x -> x - 1)
+
+-- Функция проверки условия (счетчик больше 0)
+checkCondition :: CounterMonad Bool
+checkCondition = gets (> 0)
+
+-- Пример использования whileM_
+example :: CounterMonad ()
+example = whileM_ checkCondition decrementCounter
+---------------------------------------
+
+-- | 4.2. Реализуйте функцию `forM_`, являющуюуся аналогом цикла for.
+--        Перед входом в цикл вычисляется init, в итерациях цикла вычисляется body, 
+--        в конце каждой итерации цикла вычисляется nextIter, итерации идут, 
+--        пока выполняется условие cond
+--
+forM_' :: Monad m => (m (), m Bool, m ()) -> m a -> m ()
+forM_' (init', cond, nextIter) body =
+  init' >> checkCondition'
+  where
+    checkCondition' = do
+      shouldContinue <- cond
+      when shouldContinue
+       $ body
+        >> nextIter
+         >> checkCondition'
+
+-- Эту функцию я тестирую непосредственно в функции fib
+---------------------------------------
+
+-- | Окружение / контекст: имя переменной в значение
+--
+type Context = M.Map String Int
+
+-- 4.3. Реализуйте следующие функции
+
+-- | Задаёт значение переменной. 
+--   Если переменная есть в контексте, перезаписывает её значение.
+--
+setVar :: String -> Int -> State Context ()
+setVar varName value = do
+  context <- get
+  put $ M.insert varName value context
+
+-- | Увеличивает значение переменной. 
+--   Если её нет в контексте, то кидает ошибку.
+--
+incVar :: String -> Int -> State Context ()
+incVar varName increment = do
+  context <- get
+  case M.lookup varName context of
+    Just oldValue -> put $ M.insert varName (oldValue + increment) context
+    Nothing       -> error $ "Variable not found: " ++ varName
+
+-- | Достаёт из контекста значение заданной переменной.
+--   Если переменной нет в контексте, то кидает ошибку.
+--
+getVar :: String -> State Context Int
+getVar varName = do
+  context <- get
+  case M.lookup varName context of
+    Just value -> return value
+    Nothing    -> error $ "Variable not found: " ++ varName
+
+---------------------------------------
+
+-- 4.4. Перепешите один в один (насколько это возможно) представленный ниже плюсовый код, 
+--      используя монаду 'State' и функции, которые вы реализовали выше. 
+--      Напишите 3 теста, проверяющие, что ваша реализация работает корректно
+
+{- 
+int fib(int n) {
+    int prev = 0;
+    int cur = 1;
+
+    for (int i = 0; i < n; i = i + 1) {
+        int c = cur;
+        cur = prev + cur;
+        prev = cur;
+    }
+
+    return cur
+} 
+-}
+
+fib :: Int -> State Context Int
+fib n = do
+  setVar "prev" 0
+  setVar "cur" 1
+  forM_' (setVar "i" 0, getVar "i" >>= \i -> return (i < n), incVar "i" 1) $ do
+    curValue  <- getVar "cur"
+    prevValue <- getVar "prev"
+    setVar "cur" (curValue + prevValue) 
+    setVar "prev" curValue     
+  getVar "cur"
+
+-------------------------------------------------------------------------------
+
+-- 5*. State и Either (2 балла)
+--     Создайте свою монаду StateWithError, обладающую сразу двумя эффектами:
+--      a) эффектом монады 'State'  (изменяемое состояние);
+--      b) эффектои монады 'Either' (возможность завершения вычисления ошибкой)
+
+-- | Задайте тип
+--
+newtype StateWithError s e a = StateWithError (s -> Either e (a, s))
+
+
+-- Реализуйте инстанс Monad для StateWithError
+
+-- Привидите пример использования
+
+instance Functor (StateWithError s e) where
+  fmap f (StateWithError g) = StateWithError $ \s -> case g s of
+    Left e         -> Left e
+    Right (a, s')  -> Right (f a, s')
+
+instance Applicative (StateWithError s e) where
+  pure a = StateWithError $ \s -> Right (a, s)
+  StateWithError mf <*> StateWithError ma = StateWithError $ \s -> case mf s of
+    Left e          -> Left e
+    Right (f, s')   -> case ma s' of
+      Left e'       -> Left e'
+      Right (a, s'') -> Right (f a, s'')
+
+instance Monad (StateWithError s e) where
+  return = pure
+  StateWithError ma >>= f = StateWithError $ \s -> case ma s of
+    Left e          -> Left e
+    Right (a, s')   -> case f a of
+      StateWithError mb -> mb s'
+
+-- | Выполняет вычисление с начальным состоянием и возвращает результат или ошибку
+runStateWithError :: StateWithError s e a -> s -> Either e a
+runStateWithError (StateWithError f) s = case f s of
+  Left e         -> Left e
+  Right (a, _)   -> Right a
+
+-- | Получает текущее состояние
+getState :: StateWithError s e s
+getState = StateWithError $ \s -> Right (s, s)
+
+-- | Устанавливает новое состояние
+putState :: s -> StateWithError s e ()
+putState newState = StateWithError $ \_ -> Right ((), newState)
+
+-- | Бросает ошибку
+throwError :: e -> StateWithError s e a
+throwError e = StateWithError $ \_ -> Left e
+
+-- | Ловит ошибку и обрабатывает её
+catchError :: StateWithError s e a -> (e -> StateWithError s e a) -> StateWithError s e a
+catchError (StateWithError ma) handler = StateWithError $ \s -> case ma s of
+  Left e         -> let StateWithError handler' = handler e in handler' s
+  Right (a, s')  -> Right (a, s')
+
+-- Пример использования:
+
+-- Функция, которая делит два числа и ловит ошибку деления на ноль
+divideWithCatch :: Int -> Int -> StateWithError Int String Int
+divideWithCatch x y =
+  catchError (do
+    currentResult <- getState
+    putState (currentResult + 1)  -- Модифицируем состояние для примера
+    if y == 0
+      then throwError "Division by zero!"
+      else return (x `div` y))
+  (\e -> throwError $ "Caught an error: " ++ e)
+
+
+-------------------------------------------------------------------------------
---
+++ hw9/test/TestSpec.hs
@@ -0,0 +1,138 @@
+module TestSpec (spec) where
+
+import           MyLib
+import           Control.Monad.Writer.Lazy
+import           Control.Monad.Reader()
+import           Control.Monad.State.Lazy
+import qualified Data.Map.Strict as M
+import qualified System.Random as R -- cabal install --lib  random
+import Data.Functor.Identity (Identity(..))
+
+
+import Test.Hspec
+  (
+    Spec
+  , it
+  , describe
+  , shouldBe
+  , shouldSatisfy
+  )
+
+spec :: Spec
+spec = do
+    describe "Tests" $ do
+      it "match && filterOne tests" $ do
+        let 
+          rules :: [Rule]
+          rules =
+            [ Rule Accept (IPAddress "192.168.1.1") (IPAddress "192.168.2.2")
+            , Rule Reject (IPAddress "10.0.0.1") (IPAddress "192.168.1.1")
+            ]
+
+          packet1, packet2 :: Packet
+          packet1 = Packet (IPAddress "192.168.1.1") (IPAddress "192.168.2.2")
+          packet2 = Packet (IPAddress "10.0.0.1") (IPAddress "192.168.1.1")
+
+        filterOne rules packet1 `shouldBe` WriterT (Identity (Just (Packet {pSource = IPAddress "192.168.1.1", pDestination = IPAddress "192.168.2.2"}),[Log {count = 1, msg = "Packet accepted: Packet {pSource = IPAddress \"192.168.1.1\", pDestination = IPAddress \"192.168.2.2\"}"}]))
+        filterOne rules packet2 `shouldBe` WriterT (Identity (Nothing,[Log {count = 1, msg = "Packet accepted: Packet {pSource = IPAddress \"10.0.0.1\", pDestination = IPAddress \"192.168.1.1\"}"}]))
+
+      it "mergeEntries tests" $ do
+        let
+          log1 = [Log 1 "Some error"]
+          log2 = [Log 2 "Some error"]
+          log3 = [Log 3 "All good"]
+
+        snd (runWriter (mergeEntries log1 log2)) `shouldBe` [Log 3 "Some error"]
+        snd (runWriter (mergeEntries log1 log3)) `shouldBe` [Log 3 "All good", Log 1 "Some error"]
+
+      it "mergeEntries tests" $ do
+        let
+          (r, _) = runWriter resultWriter
+        r `shouldBe` [2,4,6,8,10]
+
+
+      it "filterAll returns filtered packets along with logs" $ do
+        let
+          rules = [Rule Accept (IPAddress "192.168.1.1") (IPAddress "192.168.2.2")]
+          packet1 = [Packet (IPAddress "192.168.1.1") (IPAddress "192.168.2.2"), Packet (IPAddress "192.168.3.3") (IPAddress "192.168.4.4")]
+          expected = [Packet (IPAddress "192.168.1.1") (IPAddress "192.168.2.2")]
+
+        fst (runWriter (filterAll rules packet1)) `shouldBe` expected
+        fst (runWriter (filterAll rules []))      `shouldBe` []
+
+      it "lookupVar tests" $ do
+        let
+          environment1 = Env { templs = M.empty, vars = M.fromList [("AB", "CD")] }
+          environment2 = Env { templs = M.empty, vars = M.empty }
+        
+        lookupVar "AB" environment1    `shouldBe` Just "CD"
+        lookupVar "empty" environment2 `shouldBe` Nothing
+        
+      it "lookupTemplate tests" $ do
+        let
+          template1 = Text "RNA secondary structure important"
+          environment1 = Env { templs = M.singleton "RNA" template1, vars = M.empty }
+          
+        lookupTemplate "RNA" environment1  `shouldBe` Just template1
+        lookupTemplate "mRNA" environment1 `shouldBe` Nothing
+
+      it "addDefs tests" $ do
+        let 
+          testVars = M.fromList [("tRNA", "rRNA")]
+
+          environment1 = Env { templs = M.empty, vars = M.empty }
+          environment2 = Env { templs = M.empty, vars = testVars }
+
+          newVars = M.fromList [("ab", "12"), ("cd", "34")]
+
+          addEnvironment1 = addDefs newVars environment1
+          addEnvironment2 = addDefs newVars environment2
+
+          expected = M.union testVars newVars
+
+        vars addEnvironment1 `shouldBe` newVars
+        vars addEnvironment2 `shouldBe` expected
+
+      it "whileM_ tests" $ do
+        runState example 5 `shouldBe` ((),0)
+
+      it "getOne tests" $ do
+        let 
+          bounds = (1, 300) :: (Int, Int)
+          (value, _) = runState (getOne bounds) (R.mkStdGen 36 :: R.StdGen)
+        value `shouldSatisfy` (\x -> x >= fst bounds && x <= snd bounds)
+      
+      it "makeRandomValueST tests" $ do
+        makeRandomValueST (R.mkStdGen 23)  `shouldBe` makeRandomValue (R.mkStdGen 23)
+        makeRandomValueST (R.mkStdGen 50)  `shouldBe` makeRandomValue (R.mkStdGen 50)
+        makeRandomValueST (R.mkStdGen 100) `shouldBe` makeRandomValue (R.mkStdGen 100)
+
+      it "setVar tests" $ do
+        let
+          initState = M.fromList [("A", 1), ("B", 2)]
+          upState1 = execState (setVar "A" 3) initState
+          upState2 = execState (setVar "D" 4) initState
+        M.lookup "A" upState1 `shouldBe` Just 3
+        M.lookup "D" upState2 `shouldBe` Just 4
+
+      it "incVar tests" $ do
+        let
+          initState = M.fromList [("A", 1), ("B", 2)]
+          upState1 = execState (incVar "A" 3) initState
+        M.lookup "A" upState1 `shouldBe` Just 4
+
+      it "getVar tests" $ do
+        let
+          initState = M.fromList [("A", 1), ("B", 2)]
+        evalState (getVar "A") initState `shouldBe` 1
+
+      it "fib tests" $ do
+        evalState (fib 0) M.empty  `shouldBe` 1
+        evalState (fib 5) M.empty  `shouldBe` 8
+        evalState (fib 10) M.empty `shouldBe` 89
+        evalState (fib 15) M.empty `shouldBe` 987
+
+      it "State и Either tests" $ do
+        runStateWithError (divideWithCatch 10 2) 10 `shouldBe` Right 5
+        runStateWithError (divideWithCatch 0 2) 0   `shouldBe` Right 0
+        runStateWithError (divideWithCatch 10 0) 10 `shouldBe` Left "Caught an error: Division by zero!"
