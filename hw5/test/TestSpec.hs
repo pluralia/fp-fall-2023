e2edb5c34ee3d0dd1ae4cfdb8528d7cb85ca7fa6
Daniil Bakushkin <dev.daniil.bakushkin@gmail.com>
2023-12-06 07:12:23 +0300

    create project

---
+++ hw5/test/TestSpec.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module TestSpec where
+
+import Data.Bifunctor (Bifunctor (bimap))
+import qualified Data.Map.Strict as M
+import qualified Data.Text as T
+import qualified Data.Vector as V
+
+import MyLib
+import Test.Hspec (Spec, describe, it, shouldBe, shouldNotBe)
+
+spec :: Spec
+spec = do
+  -- for tests
+  let one = Succ Zero
+  let two = Succ one
+  let three = Succ two
+  let four = Succ three
+  let five = Succ four
+
+  -- HW3
+
+  describe "Eq ChurchNumber'" $ do
+    it "go" $ do
+      one == three `shouldBe` False
+      one == four `shouldNotBe` True
+      one /= three `shouldBe` True
+      one == one `shouldBe` True
