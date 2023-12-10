087b8df614d4dbdbf2e063c4d3f0e0a1de442f68
Julia Belova <rollineea@gmail.com>
2023-12-10 15:37:36 +0100

    add hw6

---
+++ src/MyHW6.hs
@@ -0,0 +1,367 @@
+module MyHW6 where
+import           Control.Applicative (Alternative (..))
+import           Data.Char           (digitToInt)
+import           Data.Foldable       (foldl')
+import           Data.Map.Strict     (Map, fromList)
+import           Parser
+
+-------------------------------------------------------------------------------
+
+-- В этой домашке вам потребуется подгружать одновременно 2 файла в ghci:
+-- src/Parser.hs и src/MyLib.hs. Это требует 2 шага:
+--
+-- ghci> :l src/Parser.hs src/MyLib.hs
+-- ghci> :m Parser MyLib
+
+-------------------------------------------------------------------------------
+
+-- 1. Простые парсеры (1,75 балл)
+
+---------------------------------------
+
+-- | 1.a Парсит новую строку (0,25 балла)
+--
+newLineP :: Parser Char
+newLineP = satisfyP (== '\n')
+
+---------------------------------------
+
+-- | 1.b Парсит целое число (0,25 балла)
+--       (какой парсер из модуля Parser можно переиспользовать?)
+--
+intP :: Parser Int
+intP = foldl' (\acc x -> acc * 10 + x) 0 `fmap` digitsP
+
+---------------------------------------
+
+-- | 1.c Парсит вещественное число формата `-?(0|[1-9]\d*).\d*` (0,75 балла)
+--
+floatP :: Parser Float
+floatP = (+)
+    <$> (fromIntegral <$> readFirst)
+    <* satisfyP (== '.')
+    <*> (((\x -> fromIntegral x / 10 ^ length (show x)) <$> intP) <|> pure 0)
+    where
+      readFirst = digitToInt <$> satisfyP (== '0') <|> intP
+
+---------------------------------------
+
+-- | 1.d Парсит заданную строку (0,25 балла)
+--
+stringP :: String -> Parser String
+stringP []       = pure []
+stringP (x : xs) = (:) <$> satisfyP (== x) <*> stringP xs
+
+---------------------------------------
+
+-- 1.e Парсит Value (0,25 баллa)
+
+data Value = IntValue Int | FloatValue Float | StringValue String
+  deriving (Eq, Show)
+
+-- Значения 'Value' получаются из текста по таким правилам:
+-- a) Целые числа превращаются в IntValue.
+-- б) Вещественные числа превращаются в FloatValue.
+-- в) Всё остальное превращается в StringValue.
+
+-- Парсинг 'Value', по сути, требует трех независимых парсеров:
+-- 1) intValueP
+-- 2) floatValueP
+-- 3) stringValueP
+
+-- Однако, мы не знаем, какой из них нам пригодится в моменте, поэтому хотим по порядку попробовать их все так,
+-- чтобы, если первый парсер зафейлился, мы бы попробовали второй и третий.
+-- Для объединения парсеров таким образом используйте оператор <|> из класса типов Alternative.
+
+-- | Например, вот так можно распарсить [int] или string
+--
+intOrFloatP :: Parser (Either [Int] String)
+intOrFloatP = Left <$> digitsP
+  <|> Right <$> symbolsP
+
+-- | Напишите парсер для 'Value'
+--
+valueP :: Parser Value
+valueP = FloatValue <$> floatP <|> IntValue <$> intP <|> StringValue <$> symbolsP
+
+-------------------------------------------------------------------------------
+
+-- 2. Парсер-комбинаторы и арифметика (1 балла)
+
+-- | Парсеры можно комбинировать
+--
+multDigitsP :: Parser Int
+multDigitsP = (*)        -- перемножаем то, что распарсили
+  <$> digitP             -- парсим цифру
+  <* spaceP              -- парсим произвольное число пробелов (и не сохраняем их! используя <*)
+  <* satisfyP (== '*')   -- парсим символ '*' (и не сохраняем его)
+  <* spaceP              -- парсим произвольное число пробелов (и не сохраняем их)
+  <*> digitP             -- парсим цифру
+
+runMultDigitsP :: Maybe (Int, String)
+runMultDigitsP = runParser multDigitsP "3 * 6" -- Just (18, "")
+
+---------------------------------------
+
+-- 2.a Парсит перемножение чисел (0,5 балла)
+
+-- | Парсер multDigitsP умеет работать только с цифрами, но не с числами
+--
+runMultIntsP' :: Maybe (Int, String)
+runMultIntsP' = runParser multDigitsP "33 * 6" -- Nothing
+
+-- | Реализуйте парсер, который умеет парсить 2 целых числа и перемножать их
+--
+multIntsP :: Parser Int
+multIntsP = (*)
+  <$> intP
+  <* spaceP
+  <* satisfyP (== '*')
+  <* spaceP
+  <*> intP
+
+
+-- | Парсит 2 вещественных числа и перемножает их
+--
+multFloatsP :: Parser Float
+multFloatsP = (*)
+  <$> floatP
+  <* spaceP
+  <* satisfyP (== '*')
+  <* spaceP
+  <*> floatP
+
+---------------------------------------
+
+-- 2.b Парсит перемножение/сложение 2 вещественных чисел (0,5 балла)
+
+-- | Зададим простейшее выражение:
+--   левый и правый операнд -- Float, операция -- Сhar
+--
+data SimpleExpr = SimpleExpr Float Char Float
+  deriving (Show, Eq)
+
+-- | Реализуйте парсер для SimpleExpr; операция может быть '+' и '*'
+--
+simpleExprP :: Parser SimpleExpr
+simpleExprP = SimpleExpr
+  <$> floatP
+  <* spaceP
+  <*> satisfyP (\a -> a == '+' || a == '*')
+  <* spaceP
+  <*> floatP
+
+-- | Более сложное выражение, в котором операция задана типом данных Operation
+--
+data Expr = Expr {
+    left  :: Float
+  , op    :: Operation
+  , right :: Float
+  } deriving (Show, Eq)
+
+data Operation = Mult | Sum
+  deriving (Show, Eq)
+
+-- | Реализуйте парсер для Expr
+--
+exprP :: Parser Expr
+exprP = Expr
+  <$> floatP
+  <* spaceP
+  <*> opP
+  <* spaceP
+  <*> floatP
+  where
+    opP :: Parser Operation
+    opP = (Sum <$ satisfyP (== '+')) <|> (Mult <$ satisfyP (== '*'))
+
+-- используем <$ потому что нам нужно оставить только конструктор: Sum или Mult, без парсеров
+
+
+-- | Реализуйте парсер, который парсит перемножение/сложение 2 вещественных чисел.
+--   Используйте exprP
+--
+sumMultFloatsP :: Parser Float
+sumMultFloatsP = f <$> exprP
+  where
+    f :: Expr -> Float
+    f expr = case op expr of
+      Sum  -> left expr + right expr
+      Mult -> left expr * right expr
+-------------------------------------------------------------------------------
+
+-- 3. fmap<n>, <$><*> и парсер-комбинаторы (0,75 балла)
+
+---------------------------------------
+
+-- 3.a | Реализуйте fmap4 через fmap -- прокомментируйте каждый шаг указанием типов (0,5 балла)
+--
+fmap4 :: Applicative f => (a -> b -> c -> d -> e) -> f a -> f b -> f c -> f d -> f e
+fmap4 f fa fb fc fd = f <$> fa <*> fb <*> fc <*> fd
+
+-- fmap :: (x -> y) -> f x -> f y
+-- <*> :: f (x -> y) -> f x -> f y
+
+-- f :: (a -> b - > c -> d -> e) = (a -> (b -> c -> d -> e))
+-- x = a, y = (b -> c -> d -> e)
+-- fmap :: (a -> (b -> c -> d -> e)) -> f a -> f (b -> c -> d -> e)
+-- fmap f :: f a -> f (b -> c -> d -> e)
+-- ffa = f <$> fa :: f (b -> c -> d -> e)
+
+-- ffa :: f (b -> c -> d -> e) = f (b -> (c -> d -> e))
+-- x = b, y = (c -> d -> e)
+-- ffa <*> :: f b -> f (c -> d -> e)
+-- ffaFb = ffa <*> fb :: f (c -> d -> e)
+
+-- ffafb :: f (c -> d -> e) = f (c -> (d -> e))
+-- x = c, y = (d -> e)
+-- ffaFb <*> :: f c -> f (d -> e)
+-- ffaFbFc = ffaFb <*> fc :: f (d -> e)
+
+-- ffaFbFc :: f (d -> e)
+-- x = d, y = e
+-- ffaFbFc <*> :: f d -> f e
+-- ffaFbFcFd = ffaFbFc <*> fd :: f e
+
+---------------------------------------
+
+-- 3.b Сравните реализацию fmap4 и multDigitsP/simpleExprP/exprP -- видите похожий паттерн?
+--     Поделитесь своими мыслями / наблюдениями на этот счет (0,25 балла)
+
+-- Паттерн похожий. В fmapX по сути мы ходим принимать функцию не в контексте и X - 1 значение
+-- в контексте, а затем последовательно применять эту функцию, в конце получим итоговое значение
+-- после применения функции в контексте.
+
+-- В наших функциях, например, в multDigitsP происходит то же самое! Мы хотим вернуть Parser `Type`,
+-- Parser - наш контекст. Для нашего контекста определены инстансы Functor и Applicative, поэтому
+-- мы можем проводить с ними все эти замечательные манипуляции.
+-- Распишем эти манипуляции и все станет ясно:
+
+-- (*) :: (Int -> Int -> Int)
+-- fmap :: (x -> y) -> Parser x -> Parser y
+-- x = Int, y = (Int -> Int)
+-- fmap f :: Parser a -> Parser (Int -> Int)
+
+-- Дальше у нас есть digitP :: Parser Int, который парсит цифру, к нему будем применять <*>
+-- p1 = f <$> digitP :: Parser (Int -> Int)
+
+-- Получили функцию в контексте (парсер), можно ее применить к значению в контексте и получить новое значение в контексте
+-- В нашем случае это значит, что после применения <$> мы получили частично примененную функцию (* digit) внутри парсера.
+-- Можем использовать <*>, чтобы применить эту функцию к значению другого парсера (новая распаршенная цифра) и получить ответ -
+-- - перемножение цифр.
+
+-- p1 <*> digitP :: Parser Int
+
+-------------------------------------------------------------------------------
+
+-- 4. Более сложные парсеры (2,25 балла)
+
+---------------------------------------
+
+-- | 4.a Парсит весь поток, пока символы потока удовлетворяют заданному условию (0,25 балла)
+--
+takeWhileP :: (Char -> Bool) -> Parser String
+takeWhileP f = some (satisfyP f)
+
+---------------------------------------
+
+-- | 4.b Парсер, который фейлится, если в потоке что-то осталось.
+--       В противном случае парсер отрабатывает успешно (0,5 балла)
+--
+eofP :: Parser ()
+eofP = Parser f
+  where
+    f :: String -> Maybe ((), String)
+    f [] = Just ((), [])
+    f _  = Nothing
+
+---------------------------------------
+
+-- | 4.c Парсер, который парсит символ lBorder, всё, что парсит переданный парсер p,
+--        а потом — правый символ rBorder. Возвращает то, что напарсил парсер p (0,5 балла)
+--
+inBetweenP :: String -> String -> Parser a -> Parser a
+inBetweenP lBorder rBorder p = stringP lBorder *> p <* stringP rBorder
+
+---------------------------------------
+
+-- 4.d Реализуйте функцию listP, которая парсит списки вида "[1, a   , bbb , 23     , -7]" (1 балл)
+
+-- | Вспомогательная функция;
+--   принимает на вход 2 парсера: первый парсит элементы, в второй -- разделители
+--
+sepByP :: Parser a -> Parser b -> Parser [a]
+sepByP p sep = (:) <$> p <*> some helpP
+  where
+    helpP = spaceP <* sep <* spaceP *> p
+
+
+-- | Функция принимает парсер, которым парсятся элементы списка
+--
+listP :: Parser a -> Parser [a]
+listP p = inBetweenP "[" "]" $ sepByP p (satisfyP (== ',')) <|> pure []
+
+-------------------------------------------------------------------------------
+
+-- 6. Парсим CSV формат (0,75 балл)
+
+-- Самый популярный формат для хранения табличных данных -- CSV (https://en.wikipedia.org/wiki/Comma-separated_values).
+
+-- | CSV представляет собой список названий колонок и список строк со значениями.
+--
+data CSV = CSV {
+    colNames :: [String] -- названия колонок в файле
+  , rows     :: [Row]    -- список строк со значениями из файла
+  }
+  deriving (Show)
+
+-- | Строка CSV представляет собой отображение названий колонок в их значения в данной строке, разделенные запятыми
+--   Eсли значения между запятыми нет, распарсить ставим Nothing
+--     x,y   --> [Just x, Just y]
+--     x,,y  --> [Just x, Nothing, Just y]
+--
+newtype Row = Row (Map String (Maybe Value))
+  deriving (Show, Eq)
+
+---------------------------------------
+
+-- | 6.a Реализуйте парсер, который парсит 'Row'.
+--   Названия колонок файла передаются аргументом (0,5 балла)
+--
+rowP :: [String] -> Parser Row
+rowP colnames =  Row . fromList . zip colnames <$> rowMaybeP
+  where
+    rowMaybeP = (:) <$> ((Just <$> valueP) <|> pure Nothing) <*> many helpP
+      where
+        helpP = spaceP <* satisfyP (== ',') <* spaceP *> ((Just <$> valueP) <|> pure Nothing)
+
+
+---------------------------------------
+
+-- | 6.b Парсер СSV (0,25 балла)
+--       На практике 7 лекции мы разобрали, как писать парсер CSV
+--       Скорпируйте его и запустите на вашем rowP -- убедитесь, что все работает
+--
+
+csvP :: Parser CSV
+csvP = Parser f
+  where
+    f :: String -> Maybe (CSV, String)
+    f s = case runParser colNamesP s of
+        Nothing             -> Nothing
+        Just (cols, s') -> case runParser (rowsP cols <|> pure []) s' of
+            Nothing           -> Nothing
+            Just (rowss, s'') -> Just (CSV cols rowss, s'')
+
+    colNamesP :: Parser [String]
+    colNamesP = sepByP symbolsP (satisfyP (== ','))
+
+    rowsP :: [String] -> Parser [Row]
+    rowsP cNames = many (satisfyP (== '\n') *> rowP cNames)
+
+testIO :: IO (Maybe (CSV, String))
+testIO = do
+    content <- readFile "files_for_parsing/test.csv"
+    return $ runParser csvP content
+
+-------------------------------------------------------------------------------
---
+++ test/TestSpec.hs
@@ -0,0 +1,155 @@
+module TestSpec (spec) where
+
+import           Data.Char                    (isDigit)
+import           Data.Map.Strict              (Map, fromList)
+
+
+import           Test.Hspec                   (Spec, describe, it, shouldBe)
+
+import           MyHW6
+import           Parser
+import           Text.ParserCombinators.ReadP (sepBy)
+
+spec :: Spec
+spec = do
+
+-- Task 1
+--
+  describe "simple parsers from task1" $ do
+    it "newLineP" $ do
+      runParser newLineP "" `shouldBe` Nothing
+      runParser newLineP "abc" `shouldBe` Nothing
+      runParser newLineP "\nabc" `shouldBe` Just ('\n', "abc")
+
+    it "intP" $ do
+      runParser intP "" `shouldBe` Nothing
+      runParser intP "abc" `shouldBe` Nothing
+      runParser intP "12abc" `shouldBe` Just (12, "abc")
+
+    it "floatP" $ do
+      runParser floatP "" `shouldBe` Nothing
+      runParser floatP "abc" `shouldBe` Nothing
+      runParser floatP "12abc" `shouldBe` Nothing
+      runParser floatP "12.1abc" `shouldBe` Just (12.1 :: Float, "abc")
+      runParser floatP "0.abc" `shouldBe` Just (0.0 :: Float, "abc")
+      runParser floatP "0.1abc" `shouldBe` Just (0.1 :: Float, "abc")
+
+
+    it "stringP" $ do
+      runParser (stringP "a") "" `shouldBe` Nothing
+      runParser (stringP "a") "abc" `shouldBe` Just ("a", "bc")
+      runParser (stringP "a") "bce" `shouldBe` Nothing
+
+    it "valueP" $ do
+      runParser valueP "" `shouldBe` Nothing
+      runParser valueP "123.abc" `shouldBe` Just (FloatValue (123.0 :: Float), "abc")
+      runParser valueP "123.123abc" `shouldBe` Just (FloatValue (123.123 :: Float), "abc")
+      runParser valueP "123abc" `shouldBe` Just (IntValue (123 :: Int), "abc")
+      runParser valueP "abc" `shouldBe` Just (StringValue ("abc" :: String), "")
+      runParser valueP "a, bc" `shouldBe` Just (StringValue ("a" :: String), ", bc")
+
+-- Task 2
+--
+    describe "task 2" $ do
+      it "multIntsP" $ do
+        runParser multIntsP "1*2"  `shouldBe` Just (2 :: Int, "")
+        runParser multIntsP "1*"  `shouldBe` Nothing
+        runParser multIntsP "*"  `shouldBe` Nothing
+        runParser multIntsP "*1"  `shouldBe` Nothing
+        runParser multIntsP "12*2"  `shouldBe` Just (24 :: Int, "")
+        runParser multIntsP "12 * 2"  `shouldBe` Just (24 :: Int, "")
+
+      it "multFloatsP" $ do
+        runParser multFloatsP "1.0*2.0"  `shouldBe` Just (2.0 :: Float, "")
+        runParser multFloatsP "1.11*"  `shouldBe` Nothing
+        runParser multFloatsP "*"  `shouldBe` Nothing
+        runParser multFloatsP "*1.11"  `shouldBe` Nothing
+        runParser multFloatsP "1.5*2. "  `shouldBe` Just (3 :: Float, " ")
+        runParser multFloatsP "0.1* 2.abc"  `shouldBe` Just (0.2 :: Float, "abc")
+
+      it "simpleExprP" $ do
+        let res1 = SimpleExpr 1.0 '*' 2.0
+        let res2 = SimpleExpr 1.0 '+' 2.0
+        let res3 = SimpleExpr 1.5 '*' 2.11
+        runParser simpleExprP "1.0*2.0 "  `shouldBe` Just (res1, " ")
+        runParser simpleExprP "1.0+2.0 "  `shouldBe` Just (res2, " ")
+        runParser simpleExprP "1.11*"  `shouldBe` Nothing
+        runParser simpleExprP "*"  `shouldBe` Nothing
+        runParser simpleExprP "*1.11"  `shouldBe` Nothing
+        runParser simpleExprP "1.5*2.11"  `shouldBe` Just (res3, "")
+
+      it "exprP" $ do
+        let res1 = Expr 1.0 Mult 2.0
+        let res2 = Expr 1.0 Sum 2.0
+        let res3 = Expr 1.5 Mult 2.11
+        runParser exprP "1.0*2.0 "  `shouldBe` Just (res1, " ")
+        runParser exprP "1.0+2.0 "  `shouldBe` Just (res2, " ")
+        runParser exprP "1.11*"  `shouldBe` Nothing
+        runParser exprP "*"  `shouldBe` Nothing
+        runParser exprP "*1.11"  `shouldBe` Nothing
+        runParser exprP "1.5*2.11"  `shouldBe` Just (res3, "")
+
+      it "sumMultFloatsP" $ do
+        runParser sumMultFloatsP "1.0*2.0 "  `shouldBe` Just (2.0, " ")
+        runParser sumMultFloatsP "1.0+2.0 "  `shouldBe` Just (3.0, " ")
+        runParser sumMultFloatsP "1.11*"  `shouldBe` Nothing
+        runParser sumMultFloatsP "*"  `shouldBe` Nothing
+        runParser sumMultFloatsP "*1.11"  `shouldBe` Nothing
+        runParser sumMultFloatsP "1.5*2.11"  `shouldBe` Just (3.165, "")
+
+-- Task 4
+--
+  describe "task 4" $ do
+    it "takeWhileP" $ do
+      runParser (takeWhileP (== 'a')) "" `shouldBe` Nothing
+      runParser (takeWhileP (== 'a')) "bcd" `shouldBe` Nothing
+      runParser (takeWhileP (== 'a')) "aaabc" `shouldBe` Just ("aaa", "bc")
+      runParser (takeWhileP isDigit) "123ABC" `shouldBe` Just ("123", "ABC")
+
+    it "eofP" $ do
+      runParser eofP "" `shouldBe` Just ((), "")
+      runParser eofP "abc" `shouldBe` Nothing
+
+    it "inBetweenP" $ do
+      runParser (inBetweenP "(" ")" intP) "(123)" `shouldBe` Just (123 :: Int, "")
+      runParser (inBetweenP "(" ")" intP) "(abc)" `shouldBe` Nothing
+      runParser (inBetweenP "(" ")" intP) "[abc)" `shouldBe` Nothing
+      runParser (inBetweenP "(" ")" digitP) "(123)" `shouldBe` Nothing
+
+    it "sepByP" $ do
+      let p1 = sepByP intP (satisfyP (== ','))
+      let p2 = sepByP floatP (satisfyP (== '|'))
+      let p3 = sepByP symbolsP (satisfyP (== ','))
+      runParser p1 "1, a   , bbb , 23     , -7" `shouldBe` Nothing
+      runParser p1 "1, 22   , 33 , 2" `shouldBe` Just ([1, 22, 33, 2] :: [Int], "")
+      runParser p1 "1,22,33,2" `shouldBe` Just ([1, 22, 33, 2] :: [Int], "")
+      runParser p2 "1.3 | 0. | 6.12" `shouldBe` Just ([1.3, 0.0, 6.12] :: [Float], "")
+      runParser p2 "1.3| 0.| 6.12aba" `shouldBe` Just ([1.3, 0.0, 6.12] :: [Float], "aba")
+      runParser p3 "col1,col2,col3\n" `shouldBe` Just (["col1", "col2", "col3"] :: [String], "\n")
+
+    it "listP" $ do
+      let p1 = listP intP
+      let p2 = listP symbolsP
+      runParser p1 "" `shouldBe` Nothing
+      runParser p1 "[]" `shouldBe` Just ([], "")
+      runParser p1 "[1,   2,  3,4, 5]" `shouldBe` Just ([1, 2, 3, 4, 5] :: [Int], "")
+      runParser p1 "[1,   2,  3,4, 5 ]" `shouldBe` Nothing
+      runParser p2 "[ha, ske   , ll]" `shouldBe` Just (["ha", "ske", "ll"] :: [String], "")
+
+    it "rowP" $ do
+      let cols1 = ["student", "grade"] :: [String]
+      let cols2 = ["Item", "Price", "Count" ]
+      let res1 = Row $ fromList [("student", Just $ StringValue "Julia"),
+                                   ("grade", Just $ IntValue 5)]
+      let res2 = Row $ fromList [("student", Nothing),("grade", Nothing)]
+      let res3 = Row $ fromList [("student", Nothing),("grade", Just $ IntValue 5)]
+      let res4 = Row $ fromList [("Item", Just $ StringValue "Apple"),
+                                 ("Price", Just $ FloatValue 99.99),
+                                 ("Count", Just $ IntValue 2)]
+
+      runParser (rowP cols1) "Julia,5" `shouldBe` Just (res1, "")
+      runParser (rowP cols1) "," `shouldBe` Just (res2, "")
+      runParser (rowP cols1) ",," `shouldBe` Just (res2, "")
+      runParser (rowP cols1) ",5" `shouldBe` Just (res3, "")
+      runParser (rowP cols1) ",," `shouldBe` Just (res2, "")
+      runParser (rowP cols2) "Apple,99.99,2" `shouldBe` Just (res4, "")
