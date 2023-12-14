1cfa0d62fc258fd087a689698d6743dff32c43e8
Ivan <113239177+paiksonn@users.noreply.github.com>
2023-12-14 12:58:06 +0300

    Update TestSpec.hs

--- test/TestSpec.hs
+++ test/TestSpec.hs
@@ -2,7 +2,16 @@ module TestSpec where
 
 import Test.Hspec
 import MyLib
+import Control.Monad.State
 
 spec :: Spec
 spec = do
-    True `shouldBe` True
+    describe "1. State" $ do
+        it "different inputs tests" $ do
+            let input1 = [Push, Coin, Push, Push, Coin]                         :: [TurnstileInput]
+                input2 = [Coin, Coin, Coin, Push, Push]                         :: [TurnstileInput]
+                input3 = [Push, Coin, Coin, Push, Push, Coin, Push, Push, Coin] :: [TurnstileInput]
+
+            runState (turnstile input1) Locked `shouldBe` ([Tut,Thank,Open,Tut,Thank], Unlocked)
+            runState (turnstile input2) Locked `shouldBe` ([Thank,Thank,Thank,Open,Tut], Locked)
+            runState (turnstile input3) Locked `shouldBe` ([Tut,Thank,Thank,Open,Tut,Thank,Open,Tut,Thank], Unlocked)
