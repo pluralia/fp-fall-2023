cb2f7a2daf3591b4cff8f49ac85783e8f59cb594
Daniil Bakushkin <dev.daniil.bakushkin@gmail.com>
2023-12-19 09:03:02 +0300

    h1

--- Parser.hs
+++ Parser.hs
@@ -1,20 +1,20 @@
 module Parser where
 
-import Data.Char           (digitToInt, isAlphaNum, isSpace, isDigit)
 import Control.Applicative (Alternative (..))
-import Data.Foldable       (foldl')
+import Data.Char (digitToInt, isAlphaNum, isDigit, isSpace)
+import Data.Foldable (foldl')
 
-newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
+newtype Parser a = Parser {runParser :: String -> Maybe (a, String)}
 
 instance Functor Parser where
-    fmap :: (a -> b) -> Parser a -> Parser b
-    fmap g aP = Parser f
-      where
-        f s = case runParser aP s of
-            Nothing      -> Nothing
-            Just (a, s') -> Just (g a, s')
+  fmap :: (a -> b) -> Parser a -> Parser b
+  fmap g aP = Parser f
+    where
+      f s = case runParser aP s of
+        Nothing -> Nothing
+        Just (a, s') -> Just (g a, s')
 
-instance Applicative Parser where 
+instance Applicative Parser where
   pure :: a -> Parser a
   pure a = Parser $ \s -> Just (a, s)
 
@@ -22,12 +22,12 @@ instance Applicative Parser where
   fP <*> aP = Parser f
     where
       f s = case runParser fP s of
-        Nothing      -> Nothing
+        Nothing -> Nothing
         Just (g, s') -> case runParser aP s' of
-          Nothing       -> Nothing
+          Nothing -> Nothing
           Just (a, s'') -> Just (g a, s'')
 
-instance Alternative Parser where 
+instance Alternative Parser where
   empty :: Parser a
   empty = Parser $ const Nothing
 
@@ -36,15 +36,16 @@ instance Alternative Parser where
     where
       f s = case runParser pA s of
         Nothing -> runParser pA' s
-        x       -> x
+        x -> x
 
 satisfyP :: (Char -> Bool) -> Parser Char
 satisfyP p = Parser f
   where
     f :: String -> Maybe (Char, String)
-    f []       = Nothing
-    f (x : xs) | p x       = Just (x, xs)
-               | otherwise = Nothing
+    f [] = Nothing
+    f (x : xs)
+      | p x = Just (x, xs)
+      | otherwise = Nothing
 
 symbolP :: Parser Char
 symbolP = satisfyP isAlphaNum
@@ -64,8 +65,9 @@ digitP = digitToInt <$> satisfyP isDigit
 digitsP :: Parser [Int]
 digitsP = some digitP
 
-
 -- | Как использовать парсер
---
 parseNumber :: Maybe ([Int], String)
 parseNumber = runParser digitsP "123 23 AB" -- Just ([1,2,3], " 23 AB")
+
+-- Atom <$> nameM <*> coordsM
+-- parser -> function that turns string into object
--- hw6.hs
+++ hw6.hs
@@ -1,8 +1,18 @@
-import           Control.Applicative (Alternative (..))
-import           Data.Foldable       (foldl')
-import           Data.Map.Strict     (Map, fromList)
-import qualified Data.Text           as T
-import           Parser
+import Control.Applicative (Alternative (..))
+import Data.Char (digitToInt, isAlphaNum)
+import Data.Foldable (foldl')
+import Data.Map.Strict (Map, fromList)
+import Data.Maybe (isNothing)
+import Data.Text qualified as T
+import Distribution.Simple.Utils (xargs)
+import Parser
+  ( Parser (..),
+    digitP,
+    digitsP,
+    satisfyP,
+    spaceP,
+    symbolsP,
+  )
 
 -------------------------------------------------------------------------------
 
@@ -18,35 +28,48 @@ import           Parser
 
 ---------------------------------------
 
+testString :: String
+testString = "123\n123"
+
 -- | 1.a Парсит новую строку (0,25 балла)
---
 newLineP :: Parser Char
-newLineP = undefined
+newLineP = satisfyP (== '\n')
 
 ---------------------------------------
 
 -- | 1.b Парсит целое число (0,25 балла)
 --       (какой парсер из модуля Parser можно переиспользовать?)
---
+-- :P, разобрался
 intP :: Parser Int
-intP = undefined
+intP = foldl' (\acc x -> acc * 10 + x) 0 <$> digitsP
+
+intP2 :: Parser Int -- парсит числа формата [1...9][0...9]+
+intP2 = foldl' (\acc x -> acc * 10 + x) 0 <$> ((:) <$> firstDigitP <*> digitsP)
+  where
+    firstDigitP :: Parser Int
+    firstDigitP = digitToInt <$> satisfyP (`elem` ['1' .. '9'])
 
 ---------------------------------------
 
 -- | 1.c Парсит вещественное число формата `-?(0|[1-9]\d*).\d*` (0,75 балла)
---
+-- :P, разобрался
 floatP :: Parser Float
-floatP = undefined
+floatP = (+) . fromIntegral <$> intP2 <* satisfyP (== ',') <*> helper
+  where
+    helper :: Parser Float
+    helper = foldl' (\acc x -> 0.1 * (acc + fromIntegral x)) 0.0 . reverse <$> digitsP
 
 ---------------------------------------
 
 -- | 1.d Парсит заданную строку (0,25 балла)
---
+-- :P, разобрался
 stringP :: String -> Parser String
-stringP str = Parser go
+stringP str = Parser (helper str)
   where
-    go :: String -> Maybe (String, String)
-    go = undefined
+    helper :: String -> String -> Maybe (String, String)
+    helper [] x = Just (str, x)
+    helper _ [] = Nothing
+    helper (s : ss) (x : xs) = if s == x then helper ss xs else Nothing
 
 ---------------------------------------
 
@@ -70,13 +93,14 @@ data Value = IntValue Int | FloatValue Float | StringValue String
 -- Для объединения парсеров таким образом используйте оператор <|> из класса типов Alternative.
 
 -- | Например, вот так можно распарсить [int] или string
---
 intOrFloatP :: Parser (Either [Int] String)
-intOrFloatP = Left <$> digitsP
-  <|> Right <$> symbolsP
+intOrFloatP =
+  Left
+    <$> digitsP
+      <|> Right
+    <$> symbolsP
 
 -- | Напишите парсер для 'Value'
---
 valueP :: Parser Value
 valueP = undefined
 
@@ -85,14 +109,14 @@ valueP = undefined
 -- 2. Парсер-комбинаторы и арифметика (1 балла)
 
 -- | Парсеры можно комбинировать
---
 multDigitsP :: Parser Int
-multDigitsP = (*)        -- перемножаем то, что распарсили
-  <$> digitP             -- парсим цифру
-  <* spaceP              -- парсим произвольное число пробелов (и не сохраняем их! используя <*)
-  <* satisfyP (== '*')   -- парсим символ '*' (и не сохраняем его)
-  <* spaceP              -- парсим произвольное число пробелов (и не сохраняем их)
-  <*> digitP             -- парсим цифру
+multDigitsP =
+  (*) -- перемножаем то, что распарсили
+    <$> digitP -- парсим цифру
+    <* spaceP -- парсим произвольное число пробелов (и не сохраняем их! используя <*)
+    <* satisfyP (== '*') -- парсим символ '*' (и не сохраняем его)
+    <* spaceP -- парсим произвольное число пробелов (и не сохраняем их)
+    <*> digitP -- парсим цифру
 
 runMultDigitsP :: Maybe (Int, String)
 runMultDigitsP = runParser multDigitsP "3 * 6" -- Just (18, "")
@@ -102,17 +126,14 @@ runMultDigitsP = runParser multDigitsP "3 * 6" -- Just (18, "")
 -- 2.a Парсит перемножение чисел (0,5 балла)
 
 -- | Парсер multDigitsP умеет работать только с цифрами, но не с числами
---
 runMultIntsP' :: Maybe (Int, String)
 runMultIntsP' = runParser multDigitsP "33 * 6" -- Nothing
 
 -- | Реализуйте парсер, который умеет парсить 2 целых числа и перемножать их
--- 
 multIntsP :: Parser Int
 multIntsP = undefined
 
 -- | Парсит 2 вещественных числа и перемножает их
--- 
 multFloatsP :: Parser Float
 multFloatsP = undefined
 
@@ -122,34 +143,30 @@ multFloatsP = undefined
 
 -- | Зададим простейшее выражение:
 --   левый и правый операнд -- Float, операция -- Сhar
---
 data SimpleExpr = SimpleExpr Float Char Float
   deriving (Show, Eq)
 
 -- | Реализуйте парсер для SimpleExpr; операция может быть '+' и '*'
---
 simpleExprP :: Parser SimpleExpr
 simpleExprP = undefined
 
 -- | Более сложное выражение, в котором операция задана типом данных Operation
---
-data Expr = Expr {
-    left  :: Float
-  , op    :: Operation
-  , right :: Float
-  } deriving (Show, Eq)
+data Expr = Expr
+  { left :: Float,
+    op :: Operation,
+    right :: Float
+  }
+  deriving (Show, Eq)
 
 data Operation = Mult | Sum
   deriving (Show, Eq)
 
 -- | Реализуйте парсер для Expr
---
 exprP :: Parser Expr
 exprP = undefined
 
 -- | Реализуйте парсер, который парсит перемножение/сложение 2 вещественных чисел.
 --   Используйте exprP
---
 sumMultFloatsP :: Parser Float
 sumMultFloatsP = undefined
 
@@ -161,7 +178,7 @@ sumMultFloatsP = undefined
 
 -- 3.a | Реализуйте fmap4 через fmap -- прокомментируйте каждый шаг указанием типов (0,5 балла)
 --
-fmap4 :: Applicative f => (a -> b -> c -> d -> e) -> f a -> f b -> f c -> f d -> f c
+fmap4 :: (Applicative f) => (a -> b -> c -> d -> e) -> f a -> f b -> f c -> f d -> f c
 fmap4 = undefined
 
 ---------------------------------------
@@ -176,7 +193,6 @@ fmap4 = undefined
 ---------------------------------------
 
 -- | 4.a Парсит весь поток, пока символы потока удовлетворяют заданному условию (0,25 балла)
---
 takeWhileP :: (Char -> Bool) -> Parser String
 takeWhileP = undefined
 
@@ -184,7 +200,6 @@ takeWhileP = undefined
 
 -- | 4.b Парсер, который фейлится, если в потоке что-то осталось.
 --       В противном случае парсер отрабатывает успешно (0,5 балла)
---
 eofP :: Parser ()
 eofP = undefined
 
@@ -192,7 +207,6 @@ eofP = undefined
 
 -- | 4.c Парсер, который парсит символ lBorder, всё, что парсит переданный парсер p,
 --        а потом — правый символ rBorder. Возвращает то, что напарсил парсер p (0,5 балла)
---
 inBetweenP :: String -> String -> Parser a -> Parser a
 inBetweenP lBorder rBorder p = undefined
 
@@ -202,12 +216,10 @@ inBetweenP lBorder rBorder p = undefined
 
 -- | Вспомогательная функция;
 --   принимает на вход 2 парсера: первый парсит элементы, в второй -- разделители
--- 
 sepByP :: Parser a -> Parser b -> Parser [a]
 sepByP p sep = undefined
 
 -- | Функция принимает парсер, которым парсятся элементы списка
---
 listP :: Parser a -> Parser [a]
 listP = undefined
 
@@ -218,25 +230,22 @@ listP = undefined
 -- Самый популярный формат для хранения табличных данных -- CSV (https://en.wikipedia.org/wiki/Comma-separated_values).
 
 -- | CSV представляет собой список названий колонок и список строк со значениями.
---
-data CSV = CSV {
-    colNames :: [String] -- названия колонок в файле
-  , rows     :: [Row]    -- список строк со значениями из файла
+data CSV = CSV
+  { colNames :: [String], -- названия колонок в файле
+    rows :: [Row] -- список строк со значениями из файла
   }
 
 -- | Строка CSV представляет собой отображение названий колонок в их значения в данной строке, разделенные запятыми
 --   Eсли значения между запятыми нет, распарсить ставим Nothing
 --     x,y   --> [Just x, Just y]
 --     x,,y  --> [Just x, Nothing, Just y]
---
 newtype Row = Row (Map String (Maybe Value))
   deriving (Show)
 
 ---------------------------------------
 
--- | 6.a Реализуйте парсер, который парсит 'Row'. 
+-- | 6.a Реализуйте парсер, который парсит 'Row'.
 --   Названия колонок файла передаются аргументом (0,5 балла)
---
 rowP :: [String] -> Parser Row
 rowP colNames = undefined
 
@@ -245,13 +254,12 @@ rowP colNames = undefined
 -- | 6.b Парсер СSV (0,25 балла)
 --       На практике 7 лекции мы разобрали, как писать парсер CSV
 --       Скорпируйте его и запустите на вашем rowP -- убедитесь, что все работает
---
 csvP :: Parser CSV
 csvP = undefined
 
 testIO :: IO (Maybe (CSV, String))
 testIO = do
-    content <- readFile "files_for_parsing/test.csv"
-    return $ runParser csvP content
+  content <- readFile "files_for_parsing/test.csv"
+  return $ runParser csvP content
 
 -------------------------------------------------------------------------------
