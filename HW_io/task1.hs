bba51a6d0beb827f4a98c55f2cc6778c3885e15c
jmkostina <113294009+jmkostina@users.noreply.github.com>
2023-12-22 15:49:21 +0300

    HW10_IO_1&2

---
+++ HW_io/task1.hs
@@ -0,0 +1,36 @@
+module Main where
+
+-- REPL (1,5 балла)
+
+-- Реализуйте скрипт, который при запуске в бесконечном цикле считывает с консоли пользователський ввод.
+-- Каждая введённая строка должна дублироваться в stdout (терминал) и в файл с названием text_copy_<N>.log,
+-- где N — номер файла с логом. Нумерация N начинается с 0.
+
+-- Если колчиество записанных строк в файл text_copy_<N>.log достигло 1000,
+-- то должен создаваться файл text_copy_<N+1>.log.
+-- Запись последующих строк должна производиться в него, а хэндл файла text_copy_<N>.log должен быть закрыт.
+
+import System.IO
+
+main :: IO ()
+main = do
+    infLoop 0 0
+
+infLoop :: Integer -> Integer -> IO()
+infLoop n count = do
+    line <- getLine
+
+    putStrLn line
+    let newCount = count + 1
+
+    let logFile = "text_copy_" <> show n <> ".log"
+    -- https://hackage.haskell.org/package/base-4.19.0.0/docs/System-IO.html#v:withFile
+    -- как with open('file.txt):
+    withFile logFile AppendMode $ \handle -> do
+        hPutStrLn handle line
+    
+    let newN = if count == 1000
+        then n + 1
+        else n
+    
+    infLoop newN newCount
