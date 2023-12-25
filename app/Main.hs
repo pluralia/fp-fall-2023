2af1561ffa8dd500019be0bdc8e811012acdcaeb
SophiaLaz <lazareva.sone4ka@yandex.ru>
2023-12-25 13:10:53 +0300

    Fix 8 number

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,18 @@
 {-# LANGUAGE InstanceSigs, TupleSections, ScopedTypeVariables #-}
 
-module MyBeautifulDB where
+module Main where
 
-import MyLib
-import Parser
-import qualified Data.Map as M
-import Control.Monad.State.Lazy
-import Control.Monad.Except
-import Control.Applicative (Alternative (..))
-import Data.Functor
-import System.IO
--- import qualified Data.Set as S
-import Data.List
+import           MyLib
+import           Parser
+import qualified Data.Map                                   as M
+import           Control.Monad.State.Lazy
+import           Control.Monad.Except
+import           Control.Applicative (Alternative (..))
+import           Data.Functor
+import           System.IO
+import qualified Data.Set                                   as S
+import           Data.List
+import           System.Environment (getArgs)
 
 -- | 8. Реализуйте функцию-программу `processUserDB`, которая:
 --        1. Сначала считывает из файла базу пользователей. 
@@ -40,17 +41,12 @@ type UserID = Int
 -- | Тип возможных прав пользователей.
 --
 data AccessRights = Read | Write | ReadAndWrite | Admin
-  deriving (Eq, Show)
+  deriving (Eq, Show, Ord)
 
 newtype MyState = DB (M.Map UserID [AccessRights])
-  deriving (Eq)
+  deriving (Eq, Show)
 
--- instance Show MyState where
---   show :: MyState -> String
---   show (DB dict) = forM_ [M.keys dict] $ \i -> do
-
-
--- ПАРСЕР файла со списком пользователей
+-- ПАРСЕРЫ
 dataBaseP :: Parser MyState
 dataBaseP = DB . M.fromList . map (, [] :: [AccessRights]) <$ spaceP <*> sepBy (satisfyP (=='\n')) intP
 
@@ -65,49 +61,39 @@ commandP =  (,)
         <*  spaceP
         <*> accessRightP
 
--- addAccessRight :: MyState -> UserID -> AccessRights -> MyState
--- addAccessRight db user right = 
---   case M.lookup user db of
---     Just _ -> M.insertWith (<>) user [right]
---     Nothing -> throwError (UserNotInDB user)
-
 data MyError = EmptyFile String         -- пустой файл базы пользователей
                 | UncorrectFile         -- ошибка при чтении файла
-                -- | UncorrectCommand      -- считывание команд с консоли
-                -- | UserNotInDB UserID    -- пользователь не найден в базе данных
-                -- | UncorrectAccessRight  -- некорректные права пользователя
+             -- | UncorrectCommand      -- считывание команд с консоли
+             -- | UserNotInDB UserID    -- пользователь не найден в базе данных
+             -- | UncorrectAccessRight  -- некорректные права пользователя
                 | ErrorWritingFile      -- на всякий случай (?) - ошибка при записи в файл
 
 instance Show MyError where
   show :: MyError -> String
   show (EmptyFile fileName) = "The file " ++ fileName ++ " with UserDB is empty!"
   show UncorrectFile        = "Error with parsing file!"
-  -- show UncorrectCommand     = "Uncorrect command!"
-  -- show (UserNotInDB user)   = "User " ++ show user ++ " not in database!"
-  -- show UncorrectAccessRight = "Uncorrect access rights!"
+-- show UncorrectCommand     = "Uncorrect command!"
+-- show (UserNotInDB user)   = "User " ++ show user ++ " not in database!"
+-- show UncorrectAccessRight = "Uncorrect access rights!"
   show ErrorWritingFile     = "I have some problem with writing on file..."
-
--- type MyErrorMonad = Either MyError
-
--- throwError ... - передача ошибки
 
 -- type LoggingStateWithErrorInIO stateType errorType resType = 
 --    ExceptT errorType (LoggerT (StateT stateType IO)) resType
 
 -- data LoggingLevel = Debug | Info | Warning | Error
 
-processUserDB :: FilePath -> FilePath -> FilePath -> LoggingStateWithErrorInIO MyState MyError ()
-processUserDB pathToInputDB _ pathToOutDB = do
+processUserDB :: FilePath -> FilePath -> LoggingStateWithErrorInIO MyState MyError ()
+processUserDB pathToInputDB pathToOutDB = do
     userDB <- lift . lift . lift $ readFile pathToInputDB
     case userDB of
       "" -> do
         log' Error "The file with UserDB is empty!"
         throwError (EmptyFile pathToInputDB)
       str -> case runParser dataBaseP str of
-        Just (db, _) -> do
+        Just (db, "") -> do
           log' Info "The file was read. User's data base was created!"
           put db
-        Nothing      -> do
+        _      -> do
           log' Error "The file couldn't be parsed!"
           throwError UncorrectFile
     -- Создали базу данных пользователей. Теперь считаем команды и раздадим права всем нуждающимся :)
@@ -115,7 +101,6 @@ processUserDB pathToInputDB _ pathToOutDB = do
     readCommands command
     myDB <- get
     lift . lift . lift $ writeDBFile pathToOutDB myDB
-    pure ()
 
     where
       readCommands :: String -> LoggingStateWithErrorInIO MyState MyError ()
@@ -133,10 +118,11 @@ processUserDB pathToInputDB _ pathToOutDB = do
                 case M.lookup userID db of -- ищем пользователя в базе данных
                   Just _  -> do
                     modify $ \_ -> DB (M.insertWith (<>) userID [userRight] db)
-                    log' Info $ "Congratulate! Add " ++ show userRight ++ "rights for user " ++ show userID ++ "."
-                  Nothing -> log' Warning $ "User " ++ show userID ++ "not in database!"
+                    log' Info $ "Congratulate! Add " ++ show userRight ++ " rights for user " ++ show userID ++ "."
+                  Nothing -> log' Warning $ "User " ++ show userID ++ " not in database!"
 
               Nothing -> do -- не смогли распарсить команду
+                _ <- pure $ putStrLn "Command was uncorrect!"
                 log' Warning $ "Command was uncorrect: " ++ command
 
             newCommand <- lift . lift . lift $ getLine
@@ -146,9 +132,38 @@ processUserDB pathToInputDB _ pathToOutDB = do
       writeDBFile pathToOutDB' (DB db) = do
         withFile pathToOutDB' WriteMode $ \h -> do
           let keys = M.keys db
-              part_results = map (\key -> "User " ++ show key ++ " have this rules: " ++ printRights (db M.! key)) keys
+              part_results = map (\key -> "User " ++ show key ++ " have this rules: " ++ printRights (S.toList $ S.fromList (db M.! key))) keys
               result = intercalate "\n" part_results
           hPutStrLn h result
 
       printRights :: [AccessRights] -> String
       printRights = foldl' (\ acc ai -> acc ++ " " ++ show ai) " "
+
+getRes :: FilePath -> FilePath -> FilePath -> IO ()
+getRes pathToInputDB pathToOutLog pathToOutDB = do
+  (Logged allLogs exceptRes', _) <- stateRes
+  case exceptRes' of
+    Left err -> withFile pathToOutLog WriteMode $ \h -> do
+      hPutStrLn h $ "Error: " ++ show err
+    Right () -> do
+      withFile pathToOutLog WriteMode $ \h -> do
+        let result' = intercalate "\n" $ map (\(lev, str) -> show lev ++ ": " ++ str) allLogs
+        hPutStrLn h result'
+
+  where
+    stateRes :: IO (Logged (Either MyError ()), MyState)
+    stateRes = runStateT loggerRes (DB M.empty) 
+    
+    loggerRes :: StateT MyState IO (Logged (Either MyError ()))
+    loggerRes = runLoggerT exceptRes
+
+    exceptRes :: LoggerT (StateT MyState IO) (Either MyError ())
+    exceptRes = runExceptT result
+
+    result :: LoggingStateWithErrorInIO MyState MyError ()
+    result = processUserDB pathToInputDB pathToOutDB
+
+main :: IO ()
+main = do
+    [path1, path2, path3] <- getArgs
+    getRes path1 path2 path3
