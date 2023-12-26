8d2e7b5f2032e2db7ecb97024cb786a9e51c2a10
Denis Tereshchenko <113333350+dotereschenko@users.noreply.github.com>
2023-12-26 21:20:17 +0300

    feat: now task2

--- io_hw/Task2.hs
+++ io_hw/Task2.hs
@@ -1,4 +1,10 @@
-module Main where
+module Task2 where
+
+import System.Environment
+import System.IO
+import Data.List.Split (wordsBy)
+import qualified Data.ByteString.Char8 as BS
+import Text.Printf
 
 -- wc (2 балла)
 
@@ -16,7 +22,17 @@ module Main where
 -- | Для представления файла как потока байт в Haskell есть тип `ByteString`.
 --   Он может пригодиться вам для подсчёта числа байт в файле.
 --
-import Data.ByteString (ByteString)
+wc :: FilePath -> IO ()
+wc file = do
+    content <- BS.readFile file
+    let bytes = BS.length content
+        lines = length $ BS.lines content
+        words = length $ wordsBy (\c -> c == ' ' || c == '\n' || c == '\t') (BS.unpack content)
+    putStrLn $ printf "%8d %8d %8d %s" lines words bytes file
 
 main :: IO ()
-main = undefined
+main = do
+    args <- getArgs
+    case args of
+        [file] -> wc file
+        _ -> putStrLn "./Task2 filename"
