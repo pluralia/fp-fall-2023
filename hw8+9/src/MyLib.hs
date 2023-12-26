98a7547eb6db4c6b7b16c8842d4505e9ed7b01a7
gvolsky <moseychuk.art@gmail.com>
2023-12-26 18:39:19 +0300

    hw9

--- hw8+9/src/MyLib.hs
+++ hw8+9/src/MyLib.hs
@@ -1,14 +1,18 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, InstanceSigs, LambdaCase #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, InstanceSigs #-} -- LambdaCase
 
 module MyLib where
 
 import           Control.Monad.Writer.Lazy
+import           Control.Monad.State.Lazy
 import           Control.Monad.Reader
 import           Data.Functor.Identity
 import qualified Data.Map.Strict as M
 import           Control.Monad (guard)
 import           Data.Monoid (Sum(..))
+import qualified System.Random as R         -- random
 
+------------------------------------------------------------------------------
+--------------------------------------HW8-------------------------------------
 ------------------------------------------------------------------------------
 
 -- 6. Do-нотация (0,5 балла)
@@ -240,4 +244,55 @@ testEvalStmts = runIdentity $ runReader' (evalStmts [x, y, z, xx, w]) M.empty
     xx = Stmt "x" $ Binary (Primary . Var $ "x") (Primary . Var $ "x")   -- xx = 4
     w = Stmt "w" $ Binary (Primary . Var $ "z") (Primary . Var $ "x")    -- w = 9
 
--------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+--------------------------------------HW9-------------------------------------
+------------------------------------------------------------------------------
+
+-- 3. State: Генерация случайного значения кастомного типа (0,75 балла)
+
+-- Чистый функциональный язык не может обновлять значения на месте
+-- Распространенной идиомой для имитации таких вычислений с сохранением состояния является
+-- "прохождение" параметра состояния через последовательность функций
+-- Рассмотрим пример
+
+data MyType = MT Int Bool Char Int
+  deriving Show
+
+-- Чтобы запустить: `makeRandomValue (mkStdGen 23)`
+
+makeRandomValue :: R.StdGen -> (MyType, R.StdGen)
+makeRandomValue g =
+    let (n,g1) = R.randomR (1, 100) g
+        (b,g2) = R.random g1
+        (c,g3) = R.randomR ('a', 'z') g2
+        (m,g4) = R.randomR (-n, n) g3
+    in (MT n b c m, g4)
+
+-- Этот подход работает, но такой код сложен в сопровождении, может содержать ошибки
+-- и быть грязным (что делает его грязным?)
+-- в коде используется много явных передач состояний
+-- Монада State скрывает потоковую передачу состояния внутри операции >>=,
+-- делая код проще для написания, чтения и модификации.
+
+-- | Возвращает случайное значение и обновляет состояние генератора случайных чисел
+--
+getAny :: (R.Random a) => State R.StdGen a
+getAny = state R.random
+
+-- | Аналогична getAny, но генерирует значение в границах
+--
+getOne :: (R.Random a) => (a, a) -> State R.StdGen a
+getOne bounds = state $ \gen ->
+    let (val, newGen) = R.randomR bounds gen
+    in (val, newGen)
+
+-- | Используя монаду State с StdGen в качестве состояния, мы можем генерировать случаные значения
+--   заданного типа, не передавая состояния генератора случайных чисел в коде вручную
+--
+makeRandomValueST :: R.StdGen -> (MyType, R.StdGen)
+makeRandomValueST = runState $ do
+    n <- getOne (1, 100)
+    b <- getOne (False, True)
+    c <- getOne ('a', 'z')
+    m <- getOne (-n, n)
+    return (MT n b c m)
